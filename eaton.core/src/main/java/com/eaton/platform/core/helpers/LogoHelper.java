0c870d1cca5f0797d91b665ed8d4322793834c3d
Jorge Meneses <jorge.meneses@razorfish.com>
2017-07-07 17:19:20 -0500

    Revert "Updated from TCS"
    This reverts commit cf58cc7c4742bbf0e76c0089538f4b35c0cec53e.

--- eaton.core/src/main/java/com/eaton/platform/core/helpers/LogoHelper.java
+++ eaton.core/src/main/java/com/eaton/platform/core/helpers/LogoHelper.java
@@ -1,75 +1,15 @@
 package com.eaton.platform.core.helpers;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.eaton.platform.core.bean.EatonAbstractUseBean;
-import com.eaton.platform.core.util.CommonUtil;
+import com.eaton.platform.core.BaseComponent;
+import com.eaton.platform.core.constants.CommonConstants;
 
-public class LogoHelper extends EatonAbstractUseBean {
+public class LogoHelper extends BaseComponent {
+
+	public String getHome(){
+		return getHomePath();
+	}
 	
-	/** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(LogoHelper.class);
-    
-    private String logoView;
-    private String logoImagePath;
-    private String logoImageAltText;
-    private String logoImageTitle;
-    private String logoLinkPath;
-    
-    private static final String HEADER_LOGO_SELECTOR = "header-logo";
-
-	@Override
-	public void setComponentValues() {
-		LOGGER.debug("Entered into setComponentValues method");
-		// local variables
-		String selector = null;
-		// get selector from header & footer passed while including linklist component
-		selector = slingRequest.getRequestPathInfo().getSelectorString();
-		String homePagePath = CommonUtil.getHomePagePath(currentPage);
-		String logoResPath = null;
-		if(StringUtils.isNotBlank(homePagePath)){
-			if(null != selector && StringUtils.equals(HEADER_LOGO_SELECTOR, selector)){
-				logoResPath = homePagePath.concat("/jcr:content/root/header/header-logo");
-			} else {
-				logoResPath = homePagePath.concat("/jcr:content/root/footer/footer-logo");
-			}
-			
-			Resource logoRes = resourceResolver.getResource(logoResPath);
-			if(logoRes!= null) {
-				ValueMap logoResVM = logoRes.getValueMap();
-				logoView = CommonUtil.getStringProperty(logoResVM, "view");
-				logoImagePath = CommonUtil.getStringProperty(logoResVM, "logoReference");
-				logoImageAltText = "Eaton Logo";
-				logoImageTitle = CommonUtil.getStringProperty(logoResVM, "transLogoTitle");
-				logoLinkPath = CommonUtil.getHomePagePath(currentPage);
-			}
-		}		
-		LOGGER.debug("Exoted from setComponentValues method");
-		
-	}
-
-	public String getLogoView() {
-		return logoView;
-	}
-
-	public String getLogoImagePath() {
-		return logoImagePath;
-	}
-
-	public String getLogoImageAltText() {
-		return logoImageAltText;
-	}
-
-	public String getLogoImageTitle() {
-		return logoImageTitle;
-	}
-
-	public String getLogoLinkPath() {
-		return logoLinkPath;
-	}
-
+	
 }
--- eaton.core/src/main/java/com/eaton/platform/core/helpers/TitleHelper.java
+++ eaton.core/src/main/java/com/eaton/platform/core/helpers/TitleHelper.java
@@ -3,11 +3,10 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.eaton.platform.core.bean.EatonAbstractUseBean;
+import com.eaton.platform.core.BaseComponent;
 import com.eaton.platform.core.constants.CommonConstants;
 import com.eaton.platform.core.models.TitleBean;
 
@@ -20,104 +19,29 @@ import com.eaton.platform.core.models.TitleBean;
  *
  */
 
-public class TitleHelper extends EatonAbstractUseBean{
+public class TitleHelper extends BaseComponent{
 	private static final Logger LOG = LoggerFactory.getLogger(TitleHelper.class);
 	TitleBean titleBean;
-	private String dateByLineEmpty = "true";
-	private String textByLineEmpty = "true";
-	private String byLineEmpty = "true";
-	
+
 	@Override
-	public void setComponentValues() {
-		titleBean = res.adaptTo(TitleBean.class);
+	public void activate() {
+		titleBean = getResource().adaptTo(TitleBean.class);
 	}
 
-	/**
-	 * 
-	 * @return titleBean
-	 */
 	public TitleBean getTitleBean() {
 		return titleBean;
 	}
 
-	/**
-	 * 
-	 * @return publication date
-	 */
 	public String getDateByLine(){
-		String pulicationDate = StringUtils.EMPTY;
 		try {
 			DateFormat publicationDateFormat = new SimpleDateFormat(CommonConstants.MANUAL_PUB_DATE_FORMAT);
-			GregorianCalendar calendar = titleBean.getByLineDate();
-			if(calendar!=null){
-				if(StringUtils.isNotBlank(calendar.toString())) {
-					pulicationDate = publicationDateFormat.format(calendar.getTime());
-				}
-			}
+			GregorianCalendar gc = titleBean.getByLineDate();
+			return publicationDateFormat.format(gc.getTime());
 		}
 		catch (Exception e) {
 			LOG.error("Exception occured due to not able to parse - ", e);
+			return null;
 		}
-		return pulicationDate;
-	}
-	
-	/**
-	 * 
-	 * @return headerTitle
-	 */
-	public String getHeaderTitle(){
-		String headerTitle = titleBean.getTitle();
-		if(null == headerTitle) {
-			headerTitle = currentPage.getNavigationTitle() != null? currentPage.getNavigationTitle() : currentPage.getPageTitle() != null ? currentPage.getPageTitle() : currentPage.getTitle();
-		}
-		return headerTitle;
-	}
-	
-	public String getTextByLine(){
-		String text = StringUtils.EMPTY;
-			text = titleBean.getTransByLineText();
-			if(StringUtils.isNotBlank(text.toString())) {
-				this.textByLineEmpty = "false";
-			}
-		return text;
-	}
-	
-	public String getByLineEmpty(){
-		if((getTextByLineEmpty().equals("false"))||(getDateByLineEmpty().equals("false"))){
-			this.byLineEmpty =  "false";
-		} else {
-			this.byLineEmpty = "true";
-		}
-		return this.byLineEmpty;
-	}
 
-	public String getDateByLineEmpty() {
-		GregorianCalendar calendar = titleBean.getByLineDate();
-		if((calendar!=null) && (StringUtils.isNotBlank(titleBean.getByLineDate().toString()))) {
-			this.dateByLineEmpty = "false";
-		} 
-		return this.dateByLineEmpty;
 	}
-
-	public void setDateByLineEmpty(String dateByLineEmpty) {
-		this.dateByLineEmpty = dateByLineEmpty;
-	}
-
-	public String getTextByLineEmpty() {
-		String text = StringUtils.EMPTY;
-		text = titleBean.getTransByLineText();
-		if(StringUtils.isNotBlank(text)) {
-			this.textByLineEmpty = "false";
-		} 
-		
-		return this.textByLineEmpty;
-	}
-
-	public void setTextByLineEmpty(String textByLineEmpty) {
-		this.textByLineEmpty = textByLineEmpty;
-	}
-	
-	
-	
-	
 }
