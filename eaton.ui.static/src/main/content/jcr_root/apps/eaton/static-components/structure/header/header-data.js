e97ddb0fff4bb2d55a40adbd9eb97f4b48ff53f8
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-11 14:39:42 -0700

    Background Image - Primary Nav (Mobile)

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/structure/header/header-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/structure/header/header-data.js
@@ -12,6 +12,7 @@ use(function () {
 
   header.primaryNavigation = {
     headline: 'We make what matters work*',
+    backgroundImage: '/content/dam/eaton/images/media-gallary_img1.png',
     links: [
       { title: 'Products', 'menuCategory': 'mega-menu-products' },
       { title: 'Services', 'menuCategory': 'mega-menu-services' },
