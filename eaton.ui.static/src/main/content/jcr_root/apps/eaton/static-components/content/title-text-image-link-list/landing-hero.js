54cbd4e1d7e952336cab33d93c583793a014837b
danyhv2 <danyhv020289>
2017-07-10 18:45:12 -0600

    add carousel

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/landing-hero.js
@@ -0,0 +1,24 @@
+"use strict";
+use(function () {
+    var data = [
+        {
+            title: 'We make what matters work2.',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac augue porta, vulputate ex id, lobortis mauris. Nunc consequat tincidunt augue eget laoreet. Donec mattis ut turpis et elementum. Vestibulum non gravida sapien. In dignissim leo eu erat consequat bibendum. Nullam bibendum sagittis nisi. Vivamus pulvinar porta sollicitudin.',
+            image: '/content/dam/eaton/images/Hero Images.png',
+            mobile_image: '/content/dam/eaton/images/Hero_Mobile.png'
+        },
+        {
+            title: 'Lorem ipsum dolor sit amet',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac augue porta, vulputate ex id, lobortis mauris. Nunc consequat tincidunt augue eget laoreet. Donec mattis ut turpis et elementum. Vestibulum non gravida sapien. In dignissim leo eu erat consequat bibendum. Nullam bibendum sagittis nisi. Vivamus pulvinar porta sollicitudin.',
+            image: '/content/dam/eaton/images/Hero Images.png',
+            mobile_image: '/content/dam/eaton/images/Hero_Mobile.png'
+        },
+        {
+            title: 'Ut ac augue porta, vulputate ex id, lobortis mauris.',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac augue porta, vulputate ex id, lobortis mauris. Nunc consequat tincidunt augue eget laoreet. Donec mattis ut turpis et elementum. Vestibulum non gravida sapien. In dignissim leo eu erat consequat bibendum. Nullam bibendum sagittis nisi. Vivamus pulvinar porta sollicitudin.',
+            image: '/content/dam/eaton/images/Hero Images.png',
+            mobile_image: '/content/dam/eaton/images/Hero_Mobile.png'
+        }
+    ];
+    return data;
+});
