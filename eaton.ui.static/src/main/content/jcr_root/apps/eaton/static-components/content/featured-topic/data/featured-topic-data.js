5c8ae655ef2df38c5db9a4f18d0202b521305fae
Steven Calderon <calderonsteven@gmail.com>
2017-07-12 18:26:28 -0500

    changed copys and aria texts

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/featured-topic/data/featured-topic-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/featured-topic/data/featured-topic-data.js
@@ -1,27 +1,27 @@
 "use strict";
 use(function () {
     var featuredTopic = {};
-    featuredTopic.title = 'Invest in something bigger';
-    featuredTopic.paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    featuredTopic.title = "Designing tomorrow's innovation, today.";
+    featuredTopic.paragraph = 'Togetjer, we can create more powerful and efficient ways to keep communities and commerce running 24/7. we have resources and tools to help you solve problems every day.';
 
     featuredTopic.leftList = {
-      title: "Stock Information",
+      title: "For design engineers",
       items: [
-        { title: 'Stock information', href: '#si' },
-        { title: 'SEC filings', href: '#sec'},
-        { title: 'Company profile', href: '#cp'} ]
+        { title: '2D/3D models', href: '#si' },
+        { title: 'Software downloads', href: '#sec'},
+        { title: 'On-demmand presentations', href: '#cp'} ]
     };
 
     featuredTopic.rightList = {
-      title: 'Financial reports',
-      itmes: [
-        { title: '2016 annual and sustainabillity report', href: '#annual' },
-        { title: '2016 Q4 quarterty report', href: '#q4'},
-        { title: 'Dividend report', href: '#report'} ]
+      title: 'For IT professionals',
+      items: [
+        { title: 'Installations guide', href: '#annual' },
+        { title: 'Software & formware downloads', href: '#q4'},
+        { title: 'IT Handbooks', href: '#report'} ]
     };
 
     featuredTopic.ctaHref = '#how-to';
-    featuredTopic.ctaText = 'See How';
+    featuredTopic.ctaText = 'View Tools & downloads';
 
     return featuredTopic;
 });
