11d47ada69a3d33c7130e93592b157e3275e23f8
Marcelo Guzman <marcelo.guzman@prodigious.cr>
2017-07-12 17:07:28 -0600

    [M-11]  page overview sass updates

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-product-overview-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-product-overview-data.js
@@ -1,11 +1,11 @@
 "use strict";
 use(function () {
     var data = {};
-    data.title = "the header";
-    data.copy = "lorem ipsum";
+    data.title = "The H1 header goes here";
+    data.copy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id vulputate tellus. Mauris vel dapibus massa, sed laoreet diam. Integer at enim augue. Pellentesque interdum ipsum velit, nec bibendum mi congue a. Vivamus dignissim, elit nec varius laoreet, justo nunc porttitor ipsum, in molestie ex dui non ante.";
     data.cta = {
         title: "my link",
-        url: "http://www.google.com",
+        url: "http://www.eaton.com",
         target: "_self",
     }
     return data;
