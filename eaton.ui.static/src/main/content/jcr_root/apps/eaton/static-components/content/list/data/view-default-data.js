047baef32f56ad60b8aab835737cf48d23d5786f
dcastill <daniel.castillo@boszdigital.com>
2017-07-12 16:48:59 -0600

    M29 Category Highlight Component

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-default-data.js
@@ -0,0 +1,13 @@
+"use strict";
+use(function () {
+    var data = {};    
+    data.leftImage = "/content/dam/eaton/images/Jet.png";
+    data.righImage = "/content/dam/eaton/images/Tractors.png";  
+    data.leftTitle = "Data Center Facility UPS";
+    data.rightTitle = "Knowledge Center"; 
+    data.leftLinkText = "Emergency power system basics: Maintaining Maintaining";
+    data.rightLinkText = "Eaton Awarded U.S. Army Corps Contract to Modernize Electrical Systems";
+    data.leftLinkURL = "#";
+    data.rightLinkURL = "#";
+    return data;
+});
