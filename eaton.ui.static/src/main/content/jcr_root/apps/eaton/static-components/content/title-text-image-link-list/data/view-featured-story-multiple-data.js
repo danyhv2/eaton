2194da9a770df59b984a7c77bce64c6a868cd1ba
danyhv2 <danyhv020289>
2017-07-11 21:53:23 -0600

    insert landing hero and featured story views

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-featured-story-multiple-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-featured-story-multiple-data.js
@@ -21,21 +21,21 @@ use(function () {
             image: '/content/dam/eaton/images/story_03.png',
             mobile_image: '/content/dam/eaton/images/FeaturedStory_Img3_mob.png',
             alt: 'Image 3'
+        },
+        {
+            title: 'Hydraulics3',
+            text: 'Augmented reality brings a new vision to hydraulic control',
+            image: '/content/dam/eaton/images/story_01.png',
+            mobile_image: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+            alt: 'Image 4'
+        },
+        {
+            title: 'Hydraulics5',
+            text: 'Augmented reality brings a new vision to hydraulic control',
+            image: '/content/dam/eaton/images/story_01.png',
+            mobile_image: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+            alt: 'Image 5'
         }
-        //{
-        //    title: 'Hydraulics3',
-        //    text: 'Augmented reality brings a new vision to hydraulic control',
-        //    image: '/content/dam/eaton/images/story_01.png',
-        //    mobile_image: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-        //    alt: 'Image 4'
-        //},
-        //{
-        //    title: 'Hydraulics5',
-        //    text: 'Augmented reality brings a new vision to hydraulic control',
-        //    image: '/content/dam/eaton/images/story_01.png',
-        //    mobile_image: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-        //    alt: 'Image 5'
-        //}
     ];
     return data;
 });
