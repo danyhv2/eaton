e978d51b61dd1f70cc1533a3faac46e26e8c9d26
Marcelo Guzman <marcelo.guzman@prodigious.cr>
2017-07-12 10:40:16 -0600

    [M-11] page overview creation code

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-product-overview-data.js
@@ -0,0 +1,13 @@
+"use strict";
+use(function () {
+    var data = {};
+    data.title = "the header";
+    data.copy = "lorem ipsum";
+    data.cta = {
+        title: "my link",
+        url: "http://www.google.com",
+        target: "_self",
+    }
+    return data;
+});
+
