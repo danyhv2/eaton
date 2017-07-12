509f7476e23131b68c6497292788d2d70b7f37a9
dcastill <daniel.castillo@boszdigital.com>
2017-07-12 16:55:07 -0600

    Small fix on data js

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-default-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-default-data.js
@@ -1,10 +1,10 @@
 "use strict";
 use(function () {
-    var data = {};    
+    var data = {};
     data.leftImage = "/content/dam/eaton/images/Jet.png";
-    data.righImage = "/content/dam/eaton/images/Tractors.png";  
+    data.righImage = "/content/dam/eaton/images/Tractors.png";
     data.leftTitle = "Data Center Facility UPS";
-    data.rightTitle = "Knowledge Center"; 
+    data.rightTitle = "Knowledge Center";
     data.leftLinkText = "Emergency power system basics: Maintaining Maintaining";
     data.rightLinkText = "Eaton Awarded U.S. Army Corps Contract to Modernize Electrical Systems";
     data.leftLinkURL = "#";
