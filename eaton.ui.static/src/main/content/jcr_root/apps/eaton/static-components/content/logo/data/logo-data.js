3004550a81992bcdb4b93b98176ff88318666bba
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-12 14:31:21 -0700

    Converting Inline Styles —> SASS + importing components (data-sly-resource)

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/logo/data/logo-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/logo/data/logo-data.js
@@ -4,7 +4,7 @@ use(function () {
   var data = {};
 
   data.imageSrc = '/content/dam/eaton/images/eaton-logo.png';
-  data.imageAlt = 'Eaton Logo Alternative Text';
+  data.imageAlt = 'Eaton Logo Alt Text';
 
   return data;
 
