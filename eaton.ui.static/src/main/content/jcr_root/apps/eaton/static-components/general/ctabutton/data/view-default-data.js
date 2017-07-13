0b89e9e3f07878caf0b6d1f7a78b6b192d255a06
Marcelo Guzman <marcelo.guzman@prodigious.cr>
2017-07-13 17:16:44 -0600

    [M-11]  refactor to create the cat button component

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/ctabutton/data/view-default-data.js
@@ -0,0 +1,13 @@
+'use strict';
+use(function () {
+
+  var data = {};
+
+    data.cta = {
+        title: "my link",
+        url: "http://www.eaton.com",
+        target: "_self"
+    }
+
+  return data;
+});
