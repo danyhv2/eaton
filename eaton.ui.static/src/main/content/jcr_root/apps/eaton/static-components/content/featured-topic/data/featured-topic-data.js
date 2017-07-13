5c4a023f4bad252b67d80a39b5e773a080c6acc2
Steven Calderon <calderonsteven@gmail.com>
2017-07-13 15:24:06 -0500

    added anchor target to main cta / fixed some lint error

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/featured-topic/data/featured-topic-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/featured-topic/data/featured-topic-data.js
@@ -5,23 +5,27 @@ use(function () {
     featuredTopic.paragraph = 'Together, we can create more powerful and efficient ways to keep communities and commerce running 24/7. we have resources and tools to help you solve problems every day.';
 
     featuredTopic.leftList = {
-      title: "For design engineers",
+      title: 'For design engineers',
       items: [
-        { title: '2D/3D models', href: '#si' },
-        { title: 'Software downloads', href: '#sec'},
-        { title: 'On-demmand presentations', href: '#cp'} ]
+        { title: '2D/3D models', href: '#si', target: '_blank' },
+        { title: 'Software downloads', href: '#sec' , target: '_blank'},
+        { title: 'On-demmand presentations', href: '#cp', target: '_blank'} ]
     };
 
     featuredTopic.rightList = {
       title: 'For IT professionals',
       items: [
-        { title: 'Installations guide', href: '#annual' },
-        { title: 'Software & formware downloads', href: '#q4'},
-        { title: 'IT Handbooks', href: '#report'} ]
+        { title: 'Installations guide', href: '#annual', target: '_blank' },
+        { title: 'Software & formware downloads', href: '#q4', target: '_blank'},
+        { title: 'IT Handbooks', href: '#report', target: '_blank'} ]
     };
 
-    featuredTopic.ctaHref = '#how-to';
-    featuredTopic.ctaText = 'View Tools & downloads';
+    featuredTopic.mainCta =  {
+      title: 'View Tools & downloads', 
+      href: '#how-to',
+      target: '_blank'
+    };
+    Href = '';
 
     return featuredTopic;
 });
