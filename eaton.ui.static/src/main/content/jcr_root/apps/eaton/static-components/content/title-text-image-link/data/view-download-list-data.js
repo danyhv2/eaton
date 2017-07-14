0012efb78e2b43a41793533528d17aa7e2327226
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-14 13:46:08 -0700

    Create data and views - Download List

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-download-list-data.js
@@ -0,0 +1,32 @@
+"use strict";
+use(function () {
+    var data = {};
+    data.headline = "Top Insights & Stories";
+    data.items = [
+      {
+        title: 'Aerospace Warranty',
+        description: 'White Paper Addresses Protection for the IE3 Revolution',
+        kind: 'PDF',
+        size: '2MB'
+      },
+      {
+        title: 'Product Name',
+        description: 'White Paper Addresses Protection for the IE3 Revolution',
+        kind: 'PDF',
+        size: '2MB'
+      },
+      {
+        title: 'Product Manual',
+        description: 'White Paper Addresses Protection for the IE3 Revolution',
+        kind: 'PDF',
+        size: '2MB'
+      },
+      {
+        title: 'Product Name Warranty',
+        description: 'White Paper Addresses Protection for the IE3 Revolution',
+        kind: 'PDF',
+        size: '2MB'
+      }
+    ]
+    return data;
+});
