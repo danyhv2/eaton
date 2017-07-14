20466f5743440509e1181bd042b9191f72ad2a15
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-14 14:46:12 -0700

    Download List (with Descriptions) - Markup  + Data

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-download-list-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-download-list-data.js
@@ -6,26 +6,30 @@ use(function () {
       {
         title: 'Aerospace Warranty',
         description: 'White Paper Addresses Protection for the IE3 Revolution',
-        kind: 'PDF',
-        size: '2MB'
+        type: 'PDF',
+        size: '2MB',
+        url: '#link-to-pdf'
       },
       {
         title: 'Product Name',
         description: 'White Paper Addresses Protection for the IE3 Revolution',
-        kind: 'PDF',
-        size: '2MB'
+        type: 'PDF',
+        size: '2MB',
+        url: '#link-to-pdf'
       },
       {
         title: 'Product Manual',
         description: 'White Paper Addresses Protection for the IE3 Revolution',
-        kind: 'PDF',
-        size: '2MB'
+        type: 'PDF',
+        size: '2MB',
+        url: '#link-to-pdf'
       },
       {
         title: 'Product Name Warranty',
         description: 'White Paper Addresses Protection for the IE3 Revolution',
-        kind: 'PDF',
-        size: '2MB'
+        type: 'PDF',
+        size: '2MB',
+        url: '#link-to-pdf'
       }
     ]
     return data;
