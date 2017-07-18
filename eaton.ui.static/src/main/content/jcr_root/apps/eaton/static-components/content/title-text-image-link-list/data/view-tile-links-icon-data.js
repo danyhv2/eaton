9471fce92faf4a2ca940877dd4ddf82000eaed10
danyhv2 <danyhv020289>
2017-07-18 11:19:40 -0600

    changes to tile links

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-tile-links-icon-data.js
@@ -0,0 +1,46 @@
+"use strict";
+use(function () {
+    var data = {
+        element: [
+            {
+                title: 'Aerospace',
+                link: 'http://www.eaton.com',
+                image: '',
+                icon: '/content/dam/eaton/images/resource.png'
+            },
+            {
+                title: 'Buildings',
+                link: 'http://www.eaton.com',
+                image: '',
+                icon: '/content/dam/eaton/images/overview.png'
+            },
+            {
+                title: 'Data centers',
+                link: 'http://www.eaton.com',
+                image: '',
+                icon: '/content/dam/eaton/images/overview.png'
+            },
+            {
+                title: 'Healthcare',
+                link: 'http://www.eaton.com',
+                image: '',
+                icon: '/content/dam/eaton/images/resource.png'
+            },
+            {
+                title: 'Machine Bulding',
+                link: 'http://www.eaton.com',
+                image: '',
+                icon: '/content/dam/eaton/images/resource.png'
+            },
+            {
+                title: 'Data Centers',
+                link: 'http://www.eaton.com',
+                image: '',
+                icon: '/content/dam/eaton/images/resource.png'
+            }
+        ],
+        header: 'Innovation gets noticed',
+        description: 'See what people are saying about Eaton.'
+    };
+    return data;
+});
