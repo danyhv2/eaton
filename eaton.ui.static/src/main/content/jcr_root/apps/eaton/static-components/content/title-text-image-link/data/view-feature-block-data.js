740aa8e4fb49a509519994bf0f0f1475585a1439
danyhv2 <danyhv020289>
2017-07-18 16:21:38 -0600

    add changes to feature block

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-data.js
@@ -1,1 +1,13 @@
-
+"use strict";
+use(function () {
+    var data = {
+        headline: 'We make big ideas work',
+        copy: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
+        ctaUrl: 'http://www.eaton.com',
+        ctaText: 'See how',
+        image: '/content/dam/eaton/images/Cityscape_mob.png',
+        alt: 'Test image',
+        backgroundColor: '#005EB8'
+    }
+    return data;
+});
