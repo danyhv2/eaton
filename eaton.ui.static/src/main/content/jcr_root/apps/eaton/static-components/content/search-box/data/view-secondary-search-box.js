964a0c02251f93c5bf107bfd1bd16ffac0f16822
Steven Calderon <calderonsteven@gmail.com>
2017-07-18 17:00:39 -0500

    search box // no prediction

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/search-box/data/view-secondary-search-box.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/search-box/data/view-secondary-search-box.js
@@ -6,6 +6,22 @@ use(function () {
       placeholder: 'Search products, arts, documentations, support, etc',
       externalLinkLabel: 'Go to external link',
       searchLabel: 'search in eaton.com'
-    }
+    };
+
+    data.results = [
+      {
+        title: 'magna aliqua',
+        href: '#'
+      },
+      {
+        title: 'consectetur adipisicing elit',
+        href: '#'
+      },
+      {
+        title: 'sed do eiusmod tempor incididunt ut labore et dolore',
+        href: '#'
+      },
+    ];
+
     return data;
 });
