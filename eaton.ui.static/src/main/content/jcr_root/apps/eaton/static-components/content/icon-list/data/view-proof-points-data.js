dec22bdf62d224d047e2c7181f8f7018ecb0196d
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-18 11:20:44 -0700

    Proof Points - Initial File Structure + Data

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
@@ -0,0 +1,22 @@
+"use strict";
+use(function () {
+    var data = {};
+    data.items = [
+      {
+        relatedText: 'of board represent diverse groups',
+        facts: '25%',
+        img: ''
+      },
+      {
+        relatedText: 'reduction of energy consumption',
+        facts: '19%',
+        img: ''
+      },
+      {
+        relatedText: 'countries served around the world',
+        facts: '175',
+        img: ''
+      }
+    ]
+    return data;
+});
