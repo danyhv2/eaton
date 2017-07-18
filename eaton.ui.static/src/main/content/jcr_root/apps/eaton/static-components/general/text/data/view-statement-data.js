2a31b4200606640371cfdfca5f69ddd447f9951b
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-18 10:19:54 -0700

    Highlight statement - Styles, Markup & data

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/text/data/view-statement-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/text/data/view-statement-data.js
@@ -3,7 +3,7 @@ use(function () {
 
   var data = {};
 
-  data.text = 'Statement Text';
+  data.text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
   return data;
 });
