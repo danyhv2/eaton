6a8353df6e575383854d651064833182044ae3c9
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-18 16:48:15 -0700

    Creating View — 6 column image only (TTIL List)

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-image-only-data.js
@@ -0,0 +1,38 @@
+"use strict";
+use(function () {
+    var data = {
+        items: [
+            {
+                image: '/content/dam/eaton/images/Hero Images.png',
+                alt: 'Image 1',
+                url: '#'
+            },
+            {
+                image: '/content/dam/eaton/images/Hero Images.png',
+                alt: 'Image 2',
+                url: '#'
+            },
+            {
+                image: '/content/dam/eaton/images/Hero Images.png',
+                alt: 'Image 3',
+                url: '#'
+            },
+            {
+                image: '/content/dam/eaton/images/Hero Images.png',
+                alt: 'Image 4',
+                url: '#'
+            },
+            {
+                image: '/content/dam/eaton/images/Hero Images.png',
+                alt: 'Image 5',
+                url: '#'
+            },
+            {
+                image: '/content/dam/eaton/images/Hero Images.png',
+                alt: 'Image 6',
+                url: '#'
+            }
+        ]
+    }
+    return data;
+});
