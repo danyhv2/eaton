bf7fbd898da95a31342a604e82c99a42edefefd5
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-18 13:25:19 -0700

    Proof Points - Styles (in progress)

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
@@ -5,17 +5,17 @@ use(function () {
       {
         relatedText: 'of board represent diverse groups',
         facts: '25%',
-        img: ''
+        img: '/content/dam/eaton/images/iconlist/icon-proof-points-1.jpg'
       },
       {
         relatedText: 'reduction of energy consumption',
         facts: '19%',
-        img: ''
+        img: '/content/dam/eaton/images/iconlist/icon-proof-points-2.jpg'
       },
       {
         relatedText: 'countries served around the world',
         facts: '175',
-        img: ''
+        img: '/content/dam/eaton/images/iconlist/icon-proof-points-3.jpg'
       }
     ]
     return data;
