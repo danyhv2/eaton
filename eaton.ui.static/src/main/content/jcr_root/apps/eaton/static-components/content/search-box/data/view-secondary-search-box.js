05ff0a4d7f67f0320183d4327daeadbccc32ebf0
Steven Calderon <calderonsteven@gmail.com>
2017-07-18 14:58:27 -0500

    added search box view to search box component

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/search-box/data/view-secondary-search-box.js
@@ -0,0 +1,11 @@
+"use strict";
+
+use(function () {
+    var data = {};
+    data.form = {
+      placeholder: 'Search products, arts, documentations, support, etc',
+      externalLinkLabel: 'Go to external link',
+      searchLabel: 'search in eaton.com'
+    }
+    return data;
+});
