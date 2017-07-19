54d495bc80a9b7c848a190defbf337ce6bc0e6d1
danyhv2 <danyhv020289>
2017-07-19 11:17:45 -0600

    more styling for article list

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-article-list-single-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-article-list-single-data.js
@@ -5,30 +5,32 @@ use(function () {
             {
                 title: 'Data Center Facility UPS',
                 text: 'Emergency power system basics',
-                url: '#',
+                url: 'http://www.eaton.com',
                 date: 'November 30, 2016'
             },
             {
                 title: 'Knowledge Center',
                 text: 'Eaton Awarded U.S. Army Corps Contract to Modernize Electrical Systems',
-                url: '#',
+                url: 'http://www.eaton.com',
                 date: 'December 12, 2015'
             },
             {
                 title: 'Healthcare',
                 text: 'Unlock power and reverse with a modern UPS',
-                url: '#',
+                url: 'http://www.eaton.com',
                 date: 'October 12, 2014'
             }
         ],
 
+        header: 'In the news',
+        description: '',
         single_feature_card: {
             eyebrow: 'Featured Category',
             headline: 'Why reality is the innovation challenge',
             image: '/content/dam/eaton/images/category_highlight_secondary_Landing.png',
             mobile_image: '/content/dam/eaton/images/category_highlight_secondary_Landing.png',
             alt: 'Featured image',
-            url: '#',
+            url: 'http://www.eaton.com',
             date: 'September 23, 2013'
         }
     }
