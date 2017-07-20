f528d34052922fa7efe609448cb84c530dac9a12
Marie de Luna <delunamarie@gmail.com>
2017-07-20 18:31:22 -0500

    Added testdata for homepage

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-l2-hero-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-l2-hero-data.js
@@ -1,8 +1,17 @@
 "use strict";
 use(function () {
     var data = {};    
-    data.headerTitle = "Eaton UPS At work";
-    data.headerText = "Discover why more companies are switching to eaton UPS.";
-    data.headerImage = "/content/dam/eaton/images/story_board.png";
+    var testdata = this.testdata;
+    
+    if ((typeof(testdata) != "undefined") && (testdata == "homepage-categoryhighlight")) {
+    	data.title = "Right now at Eaton";
+        data.text = "Discover how we make an impact every day";
+	    data.imgSrc = "/content/dam/eaton/images/story_board.png";
+    } else {
+	    data.title = "Eaton UPS At work";
+	    data.text = "Discover why more companies are switching to eaton UPS.";
+	    data.imgSrc = "/content/dam/eaton/images/story_board.png";
+    }
+    
     return data;
 });
