23c6c7ade34674869b769de53f9dcaf5896da2d4
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-20 14:18:22 -0700

    Featured List (Module) - Initial Markup + Data

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link-list/data/view-featured-list-data.js
@@ -0,0 +1,27 @@
+"use strict";
+use(function () {
+    var data = {
+        header: 'Featured Markets',
+        items: [
+            {
+                title: 'Aerospace',
+                link: 'http://www.eaton.com',
+                image: '/content/dam/eaton/images/Image1.png',
+                text: 'Motor protection for the IE3 revolution, that discusses the impact of the Energy-related Products (ErP) directive on switchgear and protection systems for electric motors.'
+            },
+            {
+                title: 'Renewables',
+                link: 'http://www.eaton.com',
+                image: '/content/dam/eaton/images/Image2.png',
+                text: 'Motor protection for the IE3 revolution, that discusses the impact of the Energy-related Products (ErP) directive on switchgear and protection systems for electric motors.'
+            },
+            {
+                title: 'Oil & Gas',
+                link: 'http://www.eaton.com',
+                image: '/content/dam/eaton/images/Image3.png',
+                text: 'Motor protection for the IE3 revolution, that discusses the impact of the Energy-related Products (ErP) directive on switchgear and protection systems for electric motors.'
+            }
+        ]
+    };
+    return data;
+});
