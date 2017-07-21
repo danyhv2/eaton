a51e97e15c07f6aecc663cbb54ff0e307c8ca008
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-20 23:27:20 -0700

    Mega Menu - Links (Flexible columns) - Markup, Data, Initial Styles

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-links-1col-data.js
@@ -0,0 +1,45 @@
+"use strict";
+use(function () {
+    var data = [
+    {
+      linkGroup: [
+        {
+          title: 'About Eaton',
+          url: '#link-1',
+          target: '_self',
+        },
+        {
+          title: 'Investor Relations',
+          url: '#link-2',
+          target: '_self',
+        },
+        {
+          title: 'News & Media',
+          url: '#link-3',
+          target: '_self',
+        },
+        {
+          title: 'Global Careers',
+          url: '#link-3',
+          target: '_self',
+        },
+        {
+          title: 'Sustainability',
+          url: '#link-3',
+          target: '_self',
+        },
+        {
+          title: 'Ethics & compliance',
+          url: '#link-3',
+          target: '_self',
+        },
+        {
+          title: 'Inclusion & Diversity',
+          url: '#link-3',
+          target: '_self',
+        }
+      ]
+    }
+  ]
+    return data;
+});
