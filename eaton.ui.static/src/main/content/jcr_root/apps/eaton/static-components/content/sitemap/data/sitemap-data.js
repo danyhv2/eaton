73d5c37bc579b77afe31d6fd9089c7d4ab0b3a58
Erick Gerardo Espinoza <gerardo.espinoza@digitaslbi.com>
2017-07-21 18:21:16 -0600

    T4: sitemap

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/sitemap/data/sitemap-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/sitemap/data/sitemap-data.js
@@ -2,98 +2,241 @@
 use(function () {
 
   var data = {
-    level1 : {
-      text : 'level1',
-      url : '#',
-      levels : [
-        {
-          text : 'level2',
-          url : '#'
-        },
-        {
-          text : 'level2',
-          url : '#',
-          levels : [
-            {
-              text: 'level3',
-              url: '#'
-            },
-            {
-              text: 'level3',
-              url: '#'
-            },
-            {
-              text: 'level3',
-              url: '#',
-              levels: [
-                {
-                  text: 'level4',
-                  url: '#'
-                },
-                {
-                  text: 'level4',
-                  url: '#',
-                  levels: [
-                    {
-                      text: 'level5',
-                      url: '#'
-                    },
-                    {
-                      text: 'level5',
-                      url: '#',
-                      levels: [
-                        {
-                          text: 'level6',
-                          url: '#'
-                        },
-                        {
-                          text: 'level6',
-                          url: '#'
-                        }
-                      ]
-                    },
-                    {
-                      text: 'level5',
-                      url: '#'
-                    }
-                  ]  
-                },
-                {
-                  text: 'level4',
-                  url: '#'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          text : 'level2',
-          url : '#'
-        },
-        {
-          text : 'level2',
-          url : '#'
-        },
-        {
-          text : 'level2',
-          url : '#'
-        },
-        {
-          text : 'level2',
-          url : '#',
-          levels: [
-            {
-              text: 'level3',
-              url: '#'
-            },
-            {
-              text: 'level3',
-              url: '#'
-            }
-          ]
-        }
-      ]
-    }
+    levels: [
+      {
+        text : 'Products',
+        url : '#',
+        levels : [
+          {
+            text : 'Automation & control',
+            url : '#',
+            levels: [
+              {
+                text: 'Switching, controlling and protecting motors',
+                url: '#',
+                levels: [
+                  {
+                    text: 'Contactors (IEC)',
+                    url: '#',
+                    levels : [
+                      {
+                        text: 'XT/DILM Contractors 3 Pole',
+                        url: '#'
+                      },
+                      {
+                        text: 'XTRE/DILA Contractors Relay',
+                        url: '#'
+                      },
+                      {
+                        text: 'XT/DILMP Contractors 4 Pole',
+                        url: '#'
+                      },
+                      {
+                        text: 'XT/DILM Contractors 3 Pole',
+                        url: '#'
+                      },
+                      {
+                        text: 'XTRE/DILA Contractors Relay',
+                        url: '#'
+                      },
+                      {
+                        text: 'XT/DILMP Contractors 4 Pole',
+                        url: '#'
+                      },
+                      {
+                        text: 'XT/DILM Contractors 3 Pole',
+                        url: '#'
+                      },
+                      {
+                        text: 'XTRE/DILA Contractors Relay',
+                        url: '#'
+                      },
+                      {
+                        text: 'XT/DILMP Contractors 4 Pole',
+                        url: '#'
+                      }
+                    ]
+                  },
+                  {
+                    text: 'Contact & Starters (NEMA & definite purpose)',
+                    url: '#',
+                    levels: [
+                      {
+                        text: 'Freedom Series',
+                        url: '#'
+                      },
+                      {
+                        text: 'A200 Series',
+                        url: '#'
+                      },
+                      {
+                        text: 'Definite Purpose Contactors Starters',
+                        url: '#'
+                      },
+                      {
+                        text: 'Freedom Series',
+                        url: '#'
+                      },
+                      {
+                        text: 'A200 Series',
+                        url: '#'
+                      },
+                      {
+                        text: 'Definite Purpose Contactors Starters',
+                        url: '#'
+                      },
+                      {
+                        text: 'Freedom Series',
+                        url: '#'
+                      },
+                      {
+                        text: 'A200 Series',
+                        url: '#'
+                      },
+                      {
+                        text: 'Definite Purpose Contactors Starters',
+                        url: '#'
+                      }
+                    ]
+                  },
+                  {
+                    text: 'Adjustable frequency drivers',
+                    url: '#',
+                    levels: [
+                      {
+                        text: 'PowerXL Adjustable Frequency Drives',
+                        url: '#',
+                        levels: [
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DA1 Advanced Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DG1 General Adjustable Frequency Drives',
+                          },
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          }
+                        ]
+                      },
+                      {
+                        text: 'PowerXL Adjustable Frequency Drives',
+                        url: '#',
+                        levels: [
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DA1 Advanced Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DG1 General Adjustable Frequency Drives',
+                          },
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          }
+                        ]
+                      },
+                      {
+                        text: 'PowerXL Adjustable Frequency Drives',
+                        url: '#',
+                        levels: [
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DA1 Advanced Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DG1 General Adjustable Frequency Drives',
+                          },
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          },
+                          {
+                            text: 'PowerXL DC1 Compact Adjustable Frequency Drives',
+                            url: '#'
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        text: 'Industry Soluctions',
+        url: '#',
+        levels: [
+          {
+            text: 'Solutions',
+            url: '#',
+            levels: [
+              {
+                text: 'Aviation',
+                url: '#'
+              },
+              {
+                text: 'Data center',
+                url: '#'
+              },
+              {
+                text: 'Food beverage',
+                url: '#'
+              },
+              {
+                text: 'Healthcare',
+                url: '#'
+              }
+            ]
+          },
+          {
+            text: 'Insights & Stories',
+            url: '#',
+            levels: [
+              {
+                text: 'Expert Articles',
+                url: '#'
+              },
+              {
+                text: 'Case studies',
+                url: '#'
+              },
+              {
+                text: 'Innovation & technology stories',
+                url: '#'
+              },
+              {
+                text: 'White papers',
+                url: '#'
+              }
+            ]
+          }
+        ]
+      }
+    ]
   };
 
   
