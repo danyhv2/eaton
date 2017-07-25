8c1098d0d5a8e69090b141d44d5843ff923022a5
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-25 14:10:12 -0700

    Update Proof Points - To remove wrapper divs surrounding text + styles

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
@@ -10,19 +10,19 @@ use(function () {
       {
         title: 'of board represent diverse groups',
         text: '25%',
-        tcsText: '<div class="icon-list-proof-points__fact">25</div><div class="icon-list-proof-points__symbol">%</div>',
+        tcsText: '25<div class="icon-list-proof-points__symbol">%</div>',
         image: '/content/dam/eaton/images/iconlist/icon-proof-points-1.jpg'
       },
       {
         title: 'reduction of energy consumption',
         text: '19%',
-        tcsText: '<div class="icon-list-proof-points__fact">19</div><div class="icon-list-proof-points__symbol">%</div>',
+        tcsText: '19<div class="icon-list-proof-points__symbol">%</div>',
         image: '/content/dam/eaton/images/iconlist/icon-proof-points-2.jpg'
       },
       {
         title: 'countries served around the world',
         text: '175',
-        tcsText: '<div class="icon-list-proof-points__fact">175</div>',
+        tcsText: '175',
         image: '/content/dam/eaton/images/iconlist/icon-proof-points-3.jpg'
       }
     ]
