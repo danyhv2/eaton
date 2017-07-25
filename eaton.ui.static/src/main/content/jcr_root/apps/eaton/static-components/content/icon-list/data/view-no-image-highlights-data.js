5fac2046ccbb3d8c431dff30450b5ec470137f94
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-25 13:04:47 -0700

    Update Proof Points - Data structure + Markup + Create initial files - Highlights

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-no-image-highlights-data.js
@@ -0,0 +1,19 @@
+"use strict";
+use(function () {
+    var data = {};
+    data.items = [
+      {
+        title: 'maximize reliability',
+        text: 'Maintenance bypass'
+      },
+      {
+        title: 'exclusive abm technology',
+        text: 'Increase life by 50<div class="icon-list-proof-points__symbol">%</div>'
+      },
+      {
+        title: 'predictpulse service',
+        text: '<div class="icon-list-proof-points__symbol">$</div>18B'
+      }
+    ]
+    return data;
+});
--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
@@ -1,23 +1,26 @@
+// Add comments - text
+
 "use strict";
 use(function () {
     var data = {};
     data.items = [
       {
-        relatedText: 'of board represent diverse groups',
-        facts: '25',
-        factSymbol: '%',
-        img: '/content/dam/eaton/images/iconlist/icon-proof-points-1.jpg'
+        title: 'of board represent diverse groups',
+        text: '25%',
+        tcsText: '25<div class="icon-list-proof-points__symbol">%</div>',
+        image: '/content/dam/eaton/images/iconlist/icon-proof-points-1.jpg'
       },
       {
-        relatedText: 'reduction of energy consumption',
-        facts: '19',
-        factSymbol: '%',
-        img: '/content/dam/eaton/images/iconlist/icon-proof-points-2.jpg'
+        title: 'reduction of energy consumption',
+        text: '19%',
+        tcsText: '19<div class="icon-list-proof-points__symbol">%</div>',
+        image: '/content/dam/eaton/images/iconlist/icon-proof-points-2.jpg'
       },
       {
-        relatedText: 'countries served around the world',
-        facts: '175',
-        img: '/content/dam/eaton/images/iconlist/icon-proof-points-3.jpg'
+        title: 'countries served around the world',
+        text: '175',
+        tcsText: '175',
+        image: '/content/dam/eaton/images/iconlist/icon-proof-points-3.jpg'
       }
     ]
     return data;
