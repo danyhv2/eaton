dfe738edc1c5250d41cb767e83415f9778ab8b7c
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-25 13:39:48 -0700

    Configure Views + Data - to match Proof Point construct

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-no-image-highlights-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-no-image-highlights-data.js
@@ -1,18 +1,26 @@
+// IconList - No Image Highlights
+// Please Note: For text - currently the custom field `tcsText` is mapped to the view. - context - html
+// This is done to demonstrate how the text will be split when it includes symbols.
+// Post integration - Final property to be used - `text`
+
 "use strict";
 use(function () {
     var data = {};
     data.items = [
       {
         title: 'maximize reliability',
-        text: 'Maintenance bypass'
+        text: 'Maintenance bypass',
+        tcsText: 'Maintenance bypass'
       },
       {
         title: 'exclusive abm technology',
-        text: 'Increase life by 50<div class="icon-list-proof-points__symbol">%</div>'
+        text: 'Increase life by 50%',
+        tcsText: 'Increase life by 50<div class="icon-list-no-image-highlights__symbol">%</div>'
       },
       {
         title: 'predictpulse service',
-        text: '<div class="icon-list-proof-points__symbol">$</div>18B'
+        text: '$18B',
+        tcsText: '<div class="icon-list-no-image-highlights__symbol">$</div>18B'
       }
     ]
     return data;
