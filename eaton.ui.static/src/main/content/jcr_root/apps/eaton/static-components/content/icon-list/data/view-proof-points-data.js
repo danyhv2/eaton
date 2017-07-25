0c7340559c71e4c47c71bd62c63b8a4116d84562
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-25 13:21:02 -0700

    Refactor Proof Point - as per feedback to demonstrate symbol styling & use uniform data-models

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/icon-list/data/view-proof-points-data.js
@@ -1,4 +1,7 @@
-// Add comments - text
+// IconList - Proof points
+// Please Note: For text - currently the custom field `tcsText` is mapped to the view. - context - html
+// This is done to demonstrate how the text will be split when it includes symbols.
+// Post integration - Final property to be used - `text`
 
 "use strict";
 use(function () {
@@ -7,19 +10,19 @@ use(function () {
       {
         title: 'of board represent diverse groups',
         text: '25%',
-        tcsText: '25<div class="icon-list-proof-points__symbol">%</div>',
+        tcsText: '<div class="icon-list-proof-points__fact">25</div><div class="icon-list-proof-points__symbol">%</div>',
         image: '/content/dam/eaton/images/iconlist/icon-proof-points-1.jpg'
       },
       {
         title: 'reduction of energy consumption',
         text: '19%',
-        tcsText: '19<div class="icon-list-proof-points__symbol">%</div>',
+        tcsText: '<div class="icon-list-proof-points__fact">19</div><div class="icon-list-proof-points__symbol">%</div>',
         image: '/content/dam/eaton/images/iconlist/icon-proof-points-2.jpg'
       },
       {
         title: 'countries served around the world',
         text: '175',
-        tcsText: '175',
+        tcsText: '<div class="icon-list-proof-points__fact">175</div>',
         image: '/content/dam/eaton/images/iconlist/icon-proof-points-3.jpg'
       }
     ]
