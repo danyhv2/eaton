200c372cf730828fb5c091fec29578f3e3ba93c2
danyhv2 <danyhv020289>
2017-07-26 10:57:53 -0600

    add styling for product features

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-features/data/view-product-features-data.js
@@ -0,0 +1,45 @@
+"use strict";
+use(function () {
+    var data = {};
+    var testdata = this.testdata;
+
+    if ((typeof(testdata) != "undefined") && (testdata == "product-features-with-links")) {
+        data = {
+            title: 'Energy Efficiency',
+            text: '<p>Built in energy metering.</p><p>Extends battery service life by 50% with ABM technology.</p>' +
+            '<p>Maximizes availability with extended battery modules for more runtime and optional maintenance bypass.</p>' +
+            '<p>Help save money and protect the environment through energy efficiency operation.</p>',
+            imgSrc: '/content/dam/eaton/images/energy.png',
+            imgAlt: 'Alt Text goes here',
+            backgroundColor: '#FFFFFF',
+            cta: {
+                url: 'http://www.eaton.com',
+                text: 'Make a bigger difference'
+            },
+            link: {
+                url: 'http://www.eaton.com',
+                text: 'Learn more about ENERGY STAR'
+            }
+        }
+    } else {
+        data = {
+            title: 'We make big ideas work',
+            text: '<p>Built in energy metering.</p><p>Extends battery service life by 50% with ABM technology.</p>' +
+            '<p>Maximizes availability with extended battery modules for more runtime and optional maintenance bypass.</p>' +
+            '<p>Help save money and protect the environment through energy efficiency operation.</p>',
+            imgSrc: '/content/dam/eaton/images/energy.png',
+            imgAlt: 'Alt Text goes here',
+            backgroundColor: '#FFFFFF',
+            cta: {
+                url: 'http://www.eaton.com',
+                text: 'Make a bigger difference'
+            },
+            link: {
+                url: 'http://www.eaton.com',
+                text: 'Learn more about ENERGY STAR'
+            }
+        }
+    }
+
+    return data;
+});
