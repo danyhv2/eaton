95bc319e73460ecf0d8bd75d46c582e25b0220ad
Supriya Bhat <supriya.bhat@razorfish.com>
2017-07-27 15:49:08 -0700

    Header Interaction - Mobile (manage animation to-from level-2 )

--- eaton.frontend/src/components/header/js/header.js
+++ eaton.frontend/src/components/header/js/header.js
@@ -28,7 +28,7 @@ App.header = (function() {
    * Close Level-1 Menu
    */
   const closeLevel1 = () => {
-    console.log('After transition ends');
+    console.log('After transition ends -- supriya');
 
     bodyEl.removeClass('nav-open');
     // TODO: Remove event listener when transition is completed
@@ -58,11 +58,9 @@ App.header = (function() {
       $primaryLinks.removeClass('active');
       $(event.currentTarget).addClass('active');
       const activeCategory = $(event.currentTarget).attr('data-menu-category');
-      bodyEl.addClass('nav-open level-2-open');
+      bodyEl.addClass('nav-open level-2-open nav-is-animating');
 
       // Highlight the active mega-menu section
-      // $megaMenuSections.removeClass('mega-menu__content--active');
-      console.log($megaMenu, $megaMenu.find(`[data-target="${ activeCategory }"]`));
       $megaMenu.find(`[data-target="${ activeCategory }"]`)
       .addClass('mega-menu__content--active')
       .siblings().removeClass('mega-menu__content--active');
@@ -83,24 +81,26 @@ App.header = (function() {
         // Check if Level 2 - open/close
         if (bodyEl.hasClass('level-2-open')) {
           // Close Level-2
-          bodyEl.removeClass('level-2-open');
-
-          // Close Level- 1 after
-          // window.setTimeout( (function() {
-          //   bodyEl.removeClass('nav-open');
-          // }), 700);
-          $('.mega-menu').on('transitionend', closeLevel1);
+          bodyEl.removeClass('nav-open');
+          setTimeout(function () {
+            bodyEl.removeClass('level-2-open nav-is-animating');
+          }, 300);
 
         } else {
           bodyEl.removeClass('nav-open');
         }
       } else {
         bodyEl.addClass('nav-open');
+      }
+    });
 
-        // Queue - After JS
-        // $('.eaton-link-list-primary-nav').on('transitionend', function() {
-        //   $primaryLinks.eq(0).focus();
-        // });
+    $('.mega-menu-title__level1-link').on('click', (event) => {
+
+      event.preventDefault();
+      if ($(window).width() <= 991) {
+        bodyEl.removeClass('level-2-open');
+      } else {
+        console.log('go to CTA');
       }
     });
 
