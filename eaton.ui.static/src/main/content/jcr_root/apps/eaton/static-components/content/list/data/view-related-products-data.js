96b4d4e4ce932f0820821d5c5021e958aa5988fc
danyhv2 <danyhv020289>
2017-07-27 17:08:23 -0600

    add validation for single carousel slide

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-related-products-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-related-products-data.js
@@ -1,79 +1,123 @@
 "use strict";
 use(function () {
-    var data = [
-        {
-          cards: [
-              {
-                  title: 'Eaton 9px Family',
-                  text: 'Eaton S-Series Enlosure',
-                  image: '/content/dam/eaton/images/product.png',
-                  image_alt: 'Image 1',
-                  link: 'http://www.eaton.com/',
-                  target: '_blank'
-              },
-              {
-                  title: 'Eaton 9px Family',
-                  text: 'Eaton FlexPDU& HotSwap MBP',
-                  image: '/content/dam/eaton/images/product2.png',
-                  image_alt: 'Image 2',
-                  link: 'http://www.eaton.com/',
-                  target: '_blank'
-              },
-              {
-                  title: 'Eaton 9px Family',
-                  text: 'Eaton S-Series Enlosure',
-                  image: '/content/dam/eaton/images/product.png',
-                  image_alt: 'Image 3',
-                  link: 'http://www.eaton.com/',
-                  target: '_blank'
-              },
-              {
-                  title: 'Eaton 9px Family',
-                  text: 'Eaton FlexPDU& HotSwap MBP',
-                  image: '/content/dam/eaton/images/product2.png',
-                  image_alt: 'Image 4',
-                  link: 'http://www.eaton.com/',
-                  target: '_blank'
-              }
-          ]
-        },
-        {
-            cards: [
-                {
-                    title: 'Eaton 9px Family',
-                    text: 'Eaton S-Series Enlosure',
-                    image: '/content/dam/eaton/images/product.png',
-                    image_alt: 'Image 5',
-                    link: 'http://www.eaton.com/',
-                    target: '_blank'
-                },
-                {
-                    title: 'Eaton 9px Family',
-                    text: 'Eaton FlexPDU& HotSwap MBP',
-                    image: '/content/dam/eaton/images/product2.png',
-                    image_alt: 'Image 6',
-                    link: 'http://www.eaton.com/',
-                    target: '_blank'
-                },
-                {
-                    title: 'Eaton 9px Family',
-                    text: 'Eaton S-Series Enlosure',
-                    image: '/content/dam/eaton/images/product.png',
-                    image_alt: 'Image 7',
-                    link: 'http://www.eaton.com/',
-                    target: '_blank'
-                },
-                {
-                    title: 'Eaton 9px Family',
-                    text: 'Eaton FlexPDU& HotSwap MBP',
-                    image: '/content/dam/eaton/images/product2.png',
-                    image_alt: 'Image 8',
-                    link: 'http://www.eaton.com/',
-                    target: '_blank'
-                }
-            ]
-        }
-    ]
+    var data = {};
+    var testdata = this.testdata;
+
+    if ((typeof(testdata) != "undefined") && (testdata == "single-carousel")) {
+        data = [
+            {
+                cards: [
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton S-Series Enlosure',
+                        image: '/content/dam/eaton/images/product.png',
+                        image_alt: 'Image 1',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton FlexPDU& HotSwap MBP',
+                        image: '/content/dam/eaton/images/product2.png',
+                        image_alt: 'Image 2',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton S-Series Enlosure',
+                        image: '/content/dam/eaton/images/product.png',
+                        image_alt: 'Image 3',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton FlexPDU& HotSwap MBP',
+                        image: '/content/dam/eaton/images/product2.png',
+                        image_alt: 'Image 4',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    }
+                ]
+            }
+        ]
+    } else {
+        data = [
+            {
+                cards: [
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton S-Series Enlosure',
+                        image: '/content/dam/eaton/images/product.png',
+                        image_alt: 'Image 1',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton FlexPDU& HotSwap MBP',
+                        image: '/content/dam/eaton/images/product2.png',
+                        image_alt: 'Image 2',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton S-Series Enlosure',
+                        image: '/content/dam/eaton/images/product.png',
+                        image_alt: 'Image 3',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton FlexPDU& HotSwap MBP',
+                        image: '/content/dam/eaton/images/product2.png',
+                        image_alt: 'Image 4',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    }
+                ]
+            },
+            {
+                cards: [
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton S-Series Enlosure',
+                        image: '/content/dam/eaton/images/product.png',
+                        image_alt: 'Image 5',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton FlexPDU& HotSwap MBP',
+                        image: '/content/dam/eaton/images/product2.png',
+                        image_alt: 'Image 6',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton S-Series Enlosure',
+                        image: '/content/dam/eaton/images/product.png',
+                        image_alt: 'Image 7',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    },
+                    {
+                        title: 'Eaton 9px Family',
+                        text: 'Eaton FlexPDU& HotSwap MBP',
+                        image: '/content/dam/eaton/images/product2.png',
+                        image_alt: 'Image 8',
+                        link: 'http://www.eaton.com/',
+                        target: '_blank'
+                    }
+                ]
+            }
+        ]
+    }
 
     return data;
 });
