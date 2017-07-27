f3600970311797a2100d0155ab6f761bd0878d21
Erick Gerardo Espinoza <gerardo.espinoza@digitaslbi.com>
2017-07-27 10:12:20 -0600

    link list 2 column

--- eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/components/tabbed-menu-list/js/tabbed-menu-list.js
+++ eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/components/tabbed-menu-list/js/tabbed-menu-list.js
@@ -6,8 +6,6 @@
 var App = App || window.App || {};
 
 App.TabbedMenuList = function () {
-  var _this = this;
-
   var $component = $('.eaton-tabbed-menu-list');
   var $titles = $component.find('.eaton-tabbed-menu-list__title');
   var $collapses = $component.find('collapse');
@@ -33,11 +31,11 @@ App.TabbedMenuList = function () {
         $(element).addClass('active');
       }
     });
-    $collapses.eq(index).addClass('active');
-    $tabs.eq(index).addClass('active');
-    $titles.find('span').removeClass('icon-sign-minus');
-    $titles.find('span').addClass('icon-sign-plus');
-    $(_this).find('span').addClass('icon-sign-minus');
+    // $collapses.eq(index).addClass('active');
+    // $tabs.eq(index).addClass('active');
+    // $titles.find('span').removeClass('icon-sign-minus');
+    // $titles.find('span').addClass('icon-sign-plus');
+    // $(this).find('span').addClass('icon-sign-minus');
     // if ($this.hasClass('mobile')) {
     // 	$tabs.tab('hide');
     // 	$('#tab' + index).tab('show');
