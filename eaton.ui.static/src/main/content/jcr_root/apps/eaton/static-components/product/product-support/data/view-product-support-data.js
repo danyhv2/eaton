5577ce4db2e992482430401eef6192b039d54cc2
danyhv2 <danyhv020289>
2017-07-28 16:01:55 -0600

    add styling to product support

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-support/data/view-product-support-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-support/data/view-product-support-data.js
@@ -5,27 +5,17 @@ use(function () {
 
     if ((typeof(testdata) != "undefined") && (testdata == "product-features-no-image")) {
         data = {
-            title: 'Energy Efficiency',
-            text: '<p>Built in energy metering.</p><p>Extends battery service life by 50% with ABM technology.</p>' +
-            '<p>Maximizes availability with extended battery modules for more runtime and optional maintenance bypass.</p>' +
-            '<p>Help save money and protect the environment through energy efficiency operation.</p>' +
-            '<p><a href="http://www.eaton.com" class="b-h4-link">Learn more about ENERGY STAR<sup>®</sup></a></p>',
-            imgSrc: '',
-            imgAlt: '',
-            backgroundColor: '#FFFFFF',
-
+            title: 'Need product support?',
+            copy: 'We are here to provide tips and helpful information when you need it most.',
+            icon: '',
+            info: '<div class="b-eyebrow-small">Technical Support Contact</div><p>Toll-free customer service</p><p>+1 800 356 7891</p>'
         }
     } else {
         data = {
-            title: 'We make big ideas work',
-            text: '<p>Built in energy metering.</p><p>Extends battery service life by 50% with ABM technology.</p>' +
-            '<p>Maximizes availability with extended battery modules for more runtime and optional maintenance bypass.</p>' +
-            '<p>Help save money and protect the environment through energy efficiency operation.</p>' +
-            '<p><a href="http://www.eaton.com" class="b-h4-link">Learn more about ENERGY STAR<sup>®</sup></a></p>',
-            imgSrc: '/content/dam/eaton/images/energy.png',
-            imgAlt: 'Alt Text goes here',
-            backgroundColor: '#FFFFFF',
-
+            title: 'Need product support?',
+            copy: 'We are here to provide tips and helpful information when you need it most.',
+            icon: '',
+            info: '<div class="b-eyebrow-small">Technical Support Contact</div><p>Toll-free customer service</p><p>+1 800 356 7891</p>'
         }
     }
 
