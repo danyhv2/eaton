5b1da9c6b84b891410eb11c48654d5aaad831dd4
Brian Bobek <brian.bobek@razorfish.com>
2017-07-31 18:17:09 -0500

    facets in progress

--- eaton.frontend/src/shared/module-faceted-navigation/js/module-faceted-navigation.js
+++ eaton.frontend/src/shared/module-faceted-navigation/js/module-faceted-navigation.js
@@ -1,3 +1,26 @@
 //-----------------------------------
 // M-62: Faceted Navigation
 //-----------------------------------
+'use strict';
+
+let App = window.App || {};
+
+function init() {
+
+}
+
+App.facets = (function() {
+  const $componentClass = $('.faceted-navigation');
+
+  const init = () => {
+    $('.faceted-navigation__header.button--reset').on('click', function(e) {
+      e.preventDefault;
+      $(this).children('.icon-sign-plus').toggleClass('u-hide');
+    });
+  };
+
+  if ($componentClass.length > 0) {
+    init();
+  }
+
+}());
--- eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/shared/module-faceted-navigation/js/module-faceted-navigation.js
+++ eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/shared/module-faceted-navigation/js/module-faceted-navigation.js
@@ -18,4 +18,23 @@
 //-----------------------------------
 // M-62: Faceted Navigation
 //-----------------------------------
-"use strict";
+'use strict';
+
+var App = window.App || {};
+
+function init() {}
+
+App.facets = function () {
+  var $componentClass = $('.faceted-navigation');
+
+  var init = function init() {
+    $('.faceted-navigation__header.button--reset').on('click', function (e) {
+      e.preventDefault;
+      $(this).children('.icon-sign-plus').toggleClass('u-hide');
+    });
+  };
+
+  if ($componentClass.length > 0) {
+    init();
+  }
+}();
