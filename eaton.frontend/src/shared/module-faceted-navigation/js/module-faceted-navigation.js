08b90cd1d0b104ad9b5908f7754ac6d6299b5975
Brian Bobek <brian.bobek@razorfish.com>
2017-07-31 23:44:43 -0500

    mobile appending

--- eaton.frontend/src/shared/module-faceted-navigation/js/module-faceted-navigation.js
+++ eaton.frontend/src/shared/module-faceted-navigation/js/module-faceted-navigation.js
@@ -7,12 +7,34 @@ let App = window.App || {};
 
 App.facets = (function() {
   const $componentClass = $('.faceted-navigation');
+  const $mobileFacets = $('.faceted-navigation__mobile-facet-container');
 
   const init = () => {
     $('.faceted-navigation__header.button--reset').on('click', function(e) {
       e.preventDefault;
       $(this).children('.icon-sign-plus').toggleClass('u-hide');
     });
+
+    if ($(window).width() < App.global.constants.GRID.MD) {
+      mobileFacets();
+    }
+
+    $(window).on('resize', function() {
+    	if ($(window).width() < App.global.constants.GRID.MD && $mobileFacets.css('display') != 'block') {
+    		App.global.utils.throttle(mobileFacets(),1000);
+    	}
+    	else {
+    		$componentClass.css('display','block');
+    		$mobileFacets.css('display','none');
+    	}
+    });
+  };
+
+  const mobileFacets = () => {
+  	// $mobileFacets.append($componentClass).css('display','block');
+  	$componentClass.clone().appendTo($mobileFacets).css('display','block');
+  	$componentClass.css('display','none');
+  	 $('.faceted-navigation__mobile-facet-container').css('display','block');
   };
 
   if ($componentClass.length > 0) {
--- eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/shared/module-faceted-navigation/js/module-faceted-navigation.js
+++ eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/shared/module-faceted-navigation/js/module-faceted-navigation.js
@@ -24,12 +24,33 @@ var App = window.App || {};
 
 App.facets = function () {
   var $componentClass = $('.faceted-navigation');
+  var $mobileFacets = $('.faceted-navigation__mobile-facet-container');
 
   var init = function init() {
     $('.faceted-navigation__header.button--reset').on('click', function (e) {
       e.preventDefault;
       $(this).children('.icon-sign-plus').toggleClass('u-hide');
     });
+
+    if ($(window).width() < App.global.constants.GRID.MD) {
+      mobileFacets();
+    }
+
+    $(window).on('resize', function () {
+      if ($(window).width() < App.global.constants.GRID.MD && $mobileFacets.css('display') != 'block') {
+        App.global.utils.throttle(mobileFacets(), 1000);
+      } else {
+        $componentClass.css('display', 'block');
+        $mobileFacets.css('display', 'none');
+      }
+    });
+  };
+
+  var mobileFacets = function mobileFacets() {
+    // $mobileFacets.append($componentClass).css('display','block');
+    $componentClass.clone().appendTo($mobileFacets).css('display', 'block');
+    $componentClass.css('display', 'none');
+    $('.faceted-navigation__mobile-facet-container').css('display', 'block');
   };
 
   if ($componentClass.length > 0) {
