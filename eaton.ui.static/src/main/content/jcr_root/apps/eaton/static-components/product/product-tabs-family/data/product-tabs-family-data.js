b75306a505f350e06cc0a33dd0bcd16d0ee71099
Eduard Calderon <educalde@cobogpgp1323.global.publicisgroupe.net>
2017-08-03 15:42:17 -0500

    EATON-322 and EATON-321 BUGFIX

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-tabs-family/data/product-tabs-family-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-tabs-family/data/product-tabs-family-data.js
@@ -67,12 +67,12 @@ use(function () {
           {
             icon: 'phone',
             title: 'Call toll-free 1 800 365 5794',
-            href: 'tel:18003655794',
+            href: '#18003655794',
             target: '_blank'
           },
           {
             icon: 'tiles',
-            title: 'live chat',
+            title: 'Live chat',
             href: '#live-chat',
             target: '_blank'
           }
@@ -146,12 +146,12 @@ use(function () {
           {
             icon: 'phone',
             title: 'Call toll-free 1 800 365 5794',
-            href: 'tel:18003655794',
+            href: '#18003655794',
             target: '_blank'
           },
           {
             icon: 'tiles',
-            title: 'live chat',
+            title: 'Live chat',
             href: '#live-chat',
             target: '_blank'
           }
