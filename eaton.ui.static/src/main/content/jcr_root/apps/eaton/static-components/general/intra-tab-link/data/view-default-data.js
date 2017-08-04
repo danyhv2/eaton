c441adb5ccbe7ae4c67756941d35d51d3626aa45
Eduard Calderon <educalde@cobogpgp1323.global.publicisgroupe.net>
2017-08-04 17:38:04 -0500

    added the new componet intra tab link

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/intra-tab-link/data/view-default-data.js
@@ -0,0 +1,13 @@
+'use strict';
+use(function () {
+
+  var data = {};
+
+  data.intraTab = {
+    title: 'Continue to Models',
+    href: '#continue-to-models',
+    target: '_self'
+  };
+
+  return data;
+});
