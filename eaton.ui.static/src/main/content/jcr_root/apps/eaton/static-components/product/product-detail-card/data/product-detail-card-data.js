9e7b18284249acd094ac571876079da90fdd4374
Jorge Meneses <jorge.meneses@razorfish.com>
2017-08-04 14:58:58 -0500

    [EATON-119] Adding Product Card AEM Component

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-detail-card/data/product-detail-card-data.js
@@ -0,0 +1,43 @@
+'use strict';
+use(function () {
+
+  var data = {};
+  var testdata = this.testdata;
+
+  data.productDetailCard = {
+    "title": "",
+    // TODO: Review if this component uses image-renditions
+    "imgSrc": {
+      "mobile": "http://mobile.png",
+      "tablet": "http://tablet.png",
+      "desktop": "http://desktop.png",
+    },
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "link": {
+      "text": "Request a Quote",
+      "url": "http://eaton.dev/#request-a-quote",
+      "target": "_blank"
+    }
+  };
+
+
+  data.mediaGallery = {
+    "items": [
+      {
+
+      }
+    ]
+  };
+
+
+  data.relatedProducts = {
+    "items": [
+      {
+
+      }
+    ]
+  };
+
+  return data;
+
+});
