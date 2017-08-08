b3658f3f933eee0e738b901dea75e082cf66a90e
danyhv2 <danyhv020289>
2017-08-08 14:41:06 -0600

    change feature block data

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-right-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-right-data.js
@@ -1,18 +1,39 @@
 "use strict";
 use(function () {
-    var data = {
-        title: 'We make big ideas work',
-        text: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
-        cta: {
-            url: 'http://www.eaton.com',
-            text:'Make a bigger difference'
-        },
-        imgSrc: '/content/dam/eaton/images/Cityscape_mob.png',
-        imgMobile:'/content/dam/eaton/images/Cityscape_mob.png',
-        imgTablet:'/content/dam/eaton/images/Cityscape_mob.png',
-        imgAlignment:'right',
-        imgAlt: 'Test image',
-        backgroundColor: '#005EB8'
+    var data = {};
+    var testdata = this.testdata;
+
+    if ((typeof(testdata) != "undefined") && (testdata == "left")) {
+        data = {
+            title: 'We make big ideas work',
+            text: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
+            cta: {
+                url: 'http://www.eaton.com',
+                text:'Make a bigger difference'
+            },
+            imgSrc: '/content/dam/eaton/images/Cityscape_mob.png',
+            imgMobile:'/content/dam/eaton/images/Cityscape_mob.png',
+            imgTablet:'/content/dam/eaton/images/Cityscape_mob.png',
+            imgAlignment:'left',
+            imgAlt: 'Test image',
+            backgroundColor: '#005EB8'
+        }
+    } else {
+        data = {
+            title: 'We make big ideas work',
+            text: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
+            cta: {
+                url: 'http://www.eaton.com',
+                text:'Make a bigger difference'
+            },
+            imgSrc: '/content/dam/eaton/images/Cityscape_mob.png',
+            imgMobile:'/content/dam/eaton/images/Cityscape_mob.png',
+            imgTablet:'/content/dam/eaton/images/Cityscape_mob.png',
+            imgAlignment:'right',
+            imgAlt: 'Test image',
+            backgroundColor: '#005EB8'
+        }
     }
+
     return data;
 });
