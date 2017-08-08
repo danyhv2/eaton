087823fca8ed2f2122ab65cc893fb08a520f8481
Eduard Calderon <educalde@cobogpgp1323.global.publicisgroupe.net>
2017-08-08 16:02:07 -0500

    product details tweeks

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-detail-card/data/product-detail-card-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-detail-card/data/product-detail-card-data.js
@@ -5,19 +5,38 @@ use(function () {
   var testdata = this.testdata;
 
   data.productDetailCard = {
-    "title": "",
+    "title": "9PX1500RTN",
     // TODO: Review if this component uses image-renditions
-    "imgSrc": {
-      "mobile": "http://mobile.png",
-      "tablet": "http://tablet.png",
-      "desktop": "http://desktop.png",
+    "image": {
+      "mobile": "/content/dam/eaton/images/intro_image.png",
+      "tablet": "/content/dam/eaton/images/intro_image.png",
+      "desktop": "/content/dam/eaton/images/intro_image.png",
     },
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "link": {
-      "text": "Request a Quote",
+    "price": "$9,169",
+    "cta": {
+      "title": "Request a Quote",
       "url": "http://eaton.dev/#request-a-quote",
       "target": "_blank"
-    }
+    },
+    linksHeader: 'Interact with the 9pX15000RTN',
+    links: [
+      {
+        "title": "Request a Quote",
+        "url": "http://eaton.dev/#request-a-quote",
+        "target": "_blank"
+      },
+      {
+        "title": "Request a Quote",
+        "url": "http://eaton.dev/#request-a-quote",
+        "target": "_blank"
+      },
+      {
+        "title": "Request a Quote",
+        "url": "http://eaton.dev/#request-a-quote",
+        "target": "_blank"
+      }
+    ]
   };
 
 
