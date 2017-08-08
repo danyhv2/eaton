9fbd88945f112333c15c8a073349a8e922b01f06
danyhv2 <danyhv020289>
2017-08-08 11:27:36 -0600

    add feature block left view

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-left-data.js
@@ -0,0 +1,17 @@
+"use strict";
+use(function () {
+    var data = {
+        title: 'We make big ideas work',
+        text: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
+        cta: {
+            url: 'http://www.eaton.com',
+            text:'Make a bigger difference'
+        },
+        imgSrc: '/content/dam/eaton/images/Cityscape_mob.png',
+        imgMobile:'/content/dam/eaton/images/Cityscape_mob.png',
+        imgTablet:'/content/dam/eaton/images/Cityscape_mob.png',
+        imgAlt: 'Test image',
+        backgroundColor: '#F8F8F8'
+    }
+    return data;
+});
