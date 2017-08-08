cb404751fa0ecc942f91826a7fc528406403f240
Arturo Alfaro <arturo.alfaro@prodigious.cr>
2017-08-08 17:16:42 -0600

    Fixing T7-overview template data

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-left-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/title-text-image-link/data/view-feature-block-left-data.js
@@ -18,6 +18,23 @@ use(function () {
             imgAlt: 'Test image',
             backgroundColor: '#005EB8'
         }
+   
+    } else if ((typeof(testdata) != "undefined") && (testdata == "t7-product-family-template-overview-tab")) {
+        data = {
+            title: 'Virtualization-ready for seamless integrations',
+            text: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
+            cta: {
+                url: 'http://www.eaton.com',
+                text:'Make a bigger difference'
+            },
+            imgSrc: '/content/dam/eaton/images/feature_block_desktop.png',
+            imgMobile:'/content/dam/eaton/images/feature_block_desktop.png',
+            imgTablet:'/content/dam/eaton/images/feature_block_desktop.png',
+            imgAlignment:'left',
+            imgAlt: 'Test image',
+            backgroundColor: '#005EB8'
+        }
+        
     } else {
         data = {
             title: 'We make big ideas work',
