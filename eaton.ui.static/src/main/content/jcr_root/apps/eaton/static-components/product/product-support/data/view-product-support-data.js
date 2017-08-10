3c268957e50866dc6c1325213c03cc306c3e0066
Arturo Alfaro <arturo.alfaro@prodigious.cr>
2017-08-09 18:26:45 -0600

    Fixing some templates (feedback from Marie)

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-support/data/view-product-support-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-support/data/view-product-support-data.js
@@ -13,9 +13,9 @@ use(function () {
         data = {
             title: 'Need product support?',
             copy: 'We are here to provide tips and helpful information when you need it most.',
-            info: '<h3 class="product-support-component__info--title b-eyebrow-small">Technical Support Contact</h3><div class="product-support-component__info--copy b-body-copy-small"><p>Toll-free customer service</p><p>+1 800 356 7891</p>' +
-            '<p>Voice</p><p>+1 800 859 9212</p></div>' +
-            '<div class="product-support-component__info--copy b-body-copy-small"><p>Fax</p><p>+1 800 356 7891</p><a class="b-small-link" href="mailto:test@eaton.com">test@eaton.com</a></div>'
+            info: '<p><strong>TECHNICAL SUPPORT CONTACT</strong></p><p>Toll-free customer service: +1 800 356 7891</p>' +
+            '<p>Voice: +1 800 859 9212</p>' +
+            '<p>Fax: +1 800 356 7891</p><a href="mailto:test@eaton.com">test@eaton.com</a>'
         }
     }
 
