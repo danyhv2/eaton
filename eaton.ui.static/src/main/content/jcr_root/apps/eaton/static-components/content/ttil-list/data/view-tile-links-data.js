f3b053a35cc4b7694085f1b23710df31b1ac68e4
danyhv2 <danyhv020289>
2017-08-10 11:58:31 -0600

    change test data for tile links

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil-list/data/view-tile-links-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil-list/data/view-tile-links-data.js
@@ -41,6 +41,47 @@ use(function () {
                 header: 'Innovation gets noticed',
                 description: 'See what people are saying about Eaton.'
             };    	
+    } else if ((typeof(testdata) != "undefined") && (testdata == "with-icons")) {
+        data = {
+            element: [
+                {
+                    title: 'Aerospace',
+                    link: 'http://www.eaton.com',
+                    image: '',
+                    icon: '/content/dam/eaton/images/resource.png'
+                },
+                {
+                    title: 'Buildings',
+                    link: 'http://www.eaton.com',
+                    image: '',
+                    icon: '/content/dam/eaton/images/overview.png'
+                },
+                {
+                    title: 'Data centers',
+                    link: 'http://www.eaton.com',
+                    image: '',
+                    icon: '/content/dam/eaton/images/overview.png'
+                },
+                {
+                    title: 'Healthcare',
+                    link: 'http://www.eaton.com',
+                    image: '',
+                    icon: '/content/dam/eaton/images/resource.png'
+                },
+                {
+                    title: 'Machine Bulding',
+                    link: 'http://www.eaton.com',
+                    image: '',
+                    icon: '/content/dam/eaton/images/resource.png'
+                },
+                {
+                    title: 'Data Centers',
+                    link: 'http://www.eaton.com',
+                    image: '',
+                    icon: '/content/dam/eaton/images/resource.png'
+                }
+            ]
+        }
     } else {
         data = {
                 element: [
