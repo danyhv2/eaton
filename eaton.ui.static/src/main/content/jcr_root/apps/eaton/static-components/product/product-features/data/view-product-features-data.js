b196995ddc0fa4e2bb0dd77a51309fac68a89503
danyhv2 <danyhv020289>
2017-08-11 11:13:49 -0600

    several bug fixes

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-features/data/view-product-features-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/product/product-features/data/view-product-features-data.js
@@ -57,11 +57,10 @@ use(function () {
             title: 'We make big ideas work',
             text: '<p>Built in energy metering.</p><p>Extends battery service life by 50% with ABM technology.</p>' +
             '<p>Maximizes availability with extended battery modules for more runtime and optional maintenance bypass.</p>' +
-            '<p>Help save money and protect the environment through energy efficiency operation.</p>' +
-            '<p><a href="http://www.eaton.com" class="b-h4-link">Learn more about ENERGY STAR<sup>®</sup></a></p>',
-            imgSrc: '/content/dam/eaton/images/energy.png',
-            imgMobile:'/content/dam/eaton/images/energy.png',
-            imgTablet:'/content/dam/eaton/images/energy.png',
+            '<p>Help save money and protect the environment through energy efficiency operation.</p>',
+            imgSrc: '/content/dam/eaton/images/feature_block_image.png',
+            imgMobile:'/content/dam/eaton/images/feature_block_image.png',
+            imgTablet:'/content/dam/eaton/images/feature_block_image.png',
             imgAlt: 'Alt Text goes here',
             backgroundColor: '#FFFFFF',
             cta: {
