6b00d7910492682031d1f0b86036fa4c4bedb8f0
danyhv2 <danyhv020289>
2017-08-11 11:27:34 -0600

    disable autoplay for carousel

--- eaton.frontend/src/components/list/js/list.js
+++ eaton.frontend/src/components/list/js/list.js
@@ -22,7 +22,6 @@ App.carousel = function () {
       $carousel.eq(i).slick({
         slidesToShow: $slides,
         slidesToScroll: $slides,
-        autoplay: true,
         dots: true,
         dotsClass: 'module-related-products__dots',
         prevArrow: $('.module-related-products__prev-arrow'),
