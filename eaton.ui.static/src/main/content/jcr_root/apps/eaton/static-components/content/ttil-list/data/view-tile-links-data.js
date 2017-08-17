9e5b1576ba94f2d1bd814280855078cae3133497
Arturo Alfaro <arturo.alfaro@prodigious.cr>
2017-08-17 11:38:05 -0600

    Adding T9-Careers template

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil-list/data/view-tile-links-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil-list/data/view-tile-links-data.js
@@ -58,56 +58,105 @@ use(function () {
                 ],
                 header: 'Innovation gets noticed',
                 description: 'See what people are saying about Eaton.'
-            };    	
+            };   
+    } else if ((typeof(testdata) != "undefined") && (testdata == "t9-secondary-landing-careers-1")) {
+    	data = {
+                element: [
+                    {
+                        title: 'Hiring processs',
+                        link: 'http://www.eaton.com',
+                        image: '',
+                        imageAlt: '',
+                        openNewWindow: true
+                    },
+                    {
+                        title: 'Life at Eaton',
+                        link: 'http://www.eaton.com',
+                        image: '',
+                        imageAlt: '',
+                        openNewWindow: false
+                    },
+                    {
+                        title: 'Inclusion & diversity',
+                        link: 'http://www.eaton.com',
+                        image: '',
+                        imageAlt: '',
+                        openNewWindow: true
+                    },
+                    {
+                        title: 'Culture & values',
+                        link: 'http://www.eaton.com',
+                        image: '',
+                        imageAlt: '',
+                        openNewWindow: true
+                    },
+                    {
+                        title: 'Wellness',
+                        link: 'http://www.eaton.com',
+                        image: '',
+                        imageAlt: '',
+                        openNewWindow: false
+                    },
+                    {
+                        title: 'Benefits',
+                        link: 'http://www.eaton.com',
+                        image: '',
+                        imageAlt: '',
+                        openNewWindow: true
+                    }
+                ],
+                header: 'Innovation gets noticed',
+                description: 'See what people are saying about Eaton.'
+            }; 
     }  else {
         data = {
-                element: [
-                          {
-                              title: 'Aerospace',
-                              link: 'http://www.eaton.com',
-                              image: '/content/dam/eaton/images/community.png',
-                              imageAlt: 'Test Image',
-                              openNewWindow: true
-                          },
-                          {
-                              title: 'Buildings',
-                              link: 'http://www.eaton.com',
-                              image: '/content/dam/eaton/images/community.png',
-                              imageAlt: 'Test Image',
-                              openNewWindow: true
-                          },
-                          {
-                              title: 'Data centers',
-                              link: 'http://www.eaton.com',
-                              image: '/content/dam/eaton/images/community.png',
-                              imageAlt: 'Test Image',
-                              openNewWindow: true
-                          },
-                          {
-                              title: 'Healthcare',
-                              link: 'http://www.eaton.com',
-                              image: '/content/dam/eaton/images/company-insight.png',
-                              imageAlt: 'Test Image',
-                              openNewWindow: true
-                          },
-                          {
-                              title: 'Machine',
-                              link: 'http://www.eaton.com',
-                              image: '/content/dam/eaton/images/company-insight.png',
-                              imageAlt: 'Test Image',
-                              openNewWindow: false
-                          },
-                          {
-                              title: 'Data Centers',
-                              link: 'http://www.eaton.com',
-                              image: '/content/dam/eaton/images/company-insight.png',
-                              imageAlt: 'Test Image',
-                              openNewWindow: false
-                          }
-                      ],
-                      header: 'Innovation gets noticed',
-                      description: 'See what people are saying about Eaton.'
-                 }
+            element: [
+              {
+                  title: 'Aerospace',
+                  link: 'http://www.eaton.com',
+                  image: '/content/dam/eaton/images/community.png',
+                  imageAlt: 'Test Image',
+                  openNewWindow: true
+              },
+              {
+                  title: 'Buildings',
+                  link: 'http://www.eaton.com',
+                  image: '/content/dam/eaton/images/community.png',
+                  imageAlt: 'Test Image',
+                  openNewWindow: true
+              },
+              {
+                  title: 'Data centers',
+                  link: 'http://www.eaton.com',
+                  image: '/content/dam/eaton/images/community.png',
+                  imageAlt: 'Test Image',
+                  openNewWindow: true
+              },
+              {
+                  title: 'Healthcare',
+                  link: 'http://www.eaton.com',
+                  image: '/content/dam/eaton/images/company-insight.png',
+                  imageAlt: 'Test Image',
+                  openNewWindow: true
+              },
+              {
+                  title: 'Machine',
+                  link: 'http://www.eaton.com',
+                  image: '/content/dam/eaton/images/company-insight.png',
+                  imageAlt: 'Test Image',
+                  openNewWindow: false
+              },
+              {
+                  title: 'Data Centers',
+                  link: 'http://www.eaton.com',
+                  image: '/content/dam/eaton/images/company-insight.png',
+                  imageAlt: 'Test Image',
+                  openNewWindow: false
+              }
+          ],
+                  header: 'Innovation gets noticed',
+                  description: 'See what people are saying about Eaton.'
+       }
     }
     
     return data;
