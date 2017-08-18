c66e6a339f499a3f57f127a315204ee29d63fb81
Supriya Bhat <supriya.bhat@razorfish.com>
2017-08-18 10:39:43 -0700

    [Eaton-314] CTA (Link View) - Resolve data-error & assign titles based on mega-menu pages

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/cta/data/view-link-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/cta/data/view-link-data.js
@@ -4,23 +4,24 @@ use(function () {
   var data = {};
   var testdata = this.testdata;
 
-  if ((typeof(testdata) != "undefined") && (testdata == "megamenu-products")) {
-	  data.title = 'Products';
-  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-services")) {
-	  data.title = 'Services';
-  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-markets")) {
-	  data.title = 'Markets';
-  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-support")) {
-	  data.title = 'Support';
-  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-company")) {
-	  data.title = 'Company';
-  } else {
-    data.cta = {
-      title: (properties.get('title')) ? properties.get('title') : 'CTA Link',
-      url: "http://www.eaton.com",
-      target: "_self"
-    }
+  data.cta = {
+    url: "http://www.eaton.com",
+    target: "_self"
   }
-  
+
+  if ((typeof(testdata) != "undefined") && (testdata == "megamenu-products")) {
+	  data.cta.title = 'Products';
+  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-services")) {
+	  data.cta.title = 'Services';
+  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-markets")) {
+	  data.cta.title = 'Markets';
+  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-support")) {
+	  data.cta.title = 'Support';
+  } else if ((typeof(testdata) != "undefined") && (testdata == "megamenu-company")) {
+	  data.cta.title = 'Company';
+  } else {
+    data.cta.title = (properties.get('title')) ? properties.get('title') : 'CTA Link';
+  }
+
   return data;
 });
