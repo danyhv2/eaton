a88fc3cda5caf52d2899f38baa55a85cffd488e5
Erick Gerardo Espinoza <gerardo.espinoza@digitaslbi.com>
2017-08-22 11:27:30 -0600

    EATON-421

--- eaton.frontend/src/components/tabbed-menu-list/js/tabbed-menu-list.js
+++ eaton.frontend/src/components/tabbed-menu-list/js/tabbed-menu-list.js
@@ -9,14 +9,18 @@ App.TabbedMenuList = (function() {
   const $component = $('.eaton-tabbed-menu-list');
   const $titles = $component.find('.eaton-tabbed-menu-list__title');
   const mediaquery = App.global.constants.MEDIA_QUERIES.DESKTOP;
+  const mediaquerylg = App.global.constants.MEDIA_QUERIES.DESKTOP_LG;
   const matchmedia = window.matchMedia(mediaquery);
+  const matchmedialg = window.matchMedia(mediaquerylg);
   const $collapses = $component.find('.collapse');
   const $tabsTitles = $component.find('.eaton-tabbed-menu-list__title.desktop');
   const $tabs = $component.find('.tab-pane');
+  const $tabContets = $component.find('.tab-content.desktop');
 
   const init = () => {
     bindEvents();
     matchFunction(matchmedia);
+    matchFunctionLg(matchmedialg);
   };
 
   const bindEvents = () => {
@@ -62,9 +66,50 @@ App.TabbedMenuList = (function() {
     if (matchmediaParam.matches) {// to desktop
       $collapses.collapse('hide');
 
+      if ($tabContets.length > 0) {
+        const windowWidth = $(window).outerWidth();
+        let diffWidth;
+        if (windowWidth >= App.global.constants.GRID.LG) {
+          diffWidth = windowWidth - 1140;
+        } else {
+          console.log('tab');
+          diffWidth = windowWidth - 940;
+        }
+
+        $tabContets.css({
+          'margin-left': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          'margin-right': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          width: 'calc( 100% + ' + (diffWidth + 20) + 'px)'
+        });
+        console.log('++++' + windowWidth );
+      }
+
     } else {// to mobile
       $tabsTitles.removeClass('active');
       $tabs.removeClass('active');
+    }
+  };
+
+  const matchFunctionLg = (matchmediaParam) => {
+    if (matchmediaParam.matches) {
+      if ($tabContets.length > 0) {
+        const windowWidth = $(window).outerWidth();
+        let diffWidth;
+        if (windowWidth >= App.global.constants.GRID.LG) {
+          diffWidth = windowWidth - 1140;
+        } else {
+          console.log('tab');
+          diffWidth = windowWidth - 940;
+        }
+
+        $tabContets.css({
+          'margin-left': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          'margin-right': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          width: 'calc( 100% + ' + (diffWidth + 20) + 'px)'
+        });
+        console.log('++++' + windowWidth );
+      }
+
     }
   };
 
--- eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/components/tabbed-menu-list/js/tabbed-menu-list.js
+++ eaton.ui.static/src/main/content/jcr_root/etc/designs/eaton-static/clientlib-static/components/tabbed-menu-list/js/tabbed-menu-list.js
@@ -26,14 +26,18 @@ App.TabbedMenuList = function () {
   var $component = $('.eaton-tabbed-menu-list');
   var $titles = $component.find('.eaton-tabbed-menu-list__title');
   var mediaquery = App.global.constants.MEDIA_QUERIES.DESKTOP;
+  var mediaquerylg = App.global.constants.MEDIA_QUERIES.DESKTOP_LG;
   var matchmedia = window.matchMedia(mediaquery);
+  var matchmedialg = window.matchMedia(mediaquerylg);
   var $collapses = $component.find('.collapse');
   var $tabsTitles = $component.find('.eaton-tabbed-menu-list__title.desktop');
   var $tabs = $component.find('.tab-pane');
+  var $tabContets = $component.find('.tab-content.desktop');
 
   var init = function init() {
     bindEvents();
     matchFunction(matchmedia);
+    matchFunctionLg(matchmedialg);
   };
 
   var bindEvents = function bindEvents() {
@@ -75,10 +79,50 @@ App.TabbedMenuList = function () {
     if (matchmediaParam.matches) {
       // to desktop
       $collapses.collapse('hide');
+
+      if ($tabContets.length > 0) {
+        var windowWidth = $(window).outerWidth();
+        var diffWidth = void 0;
+        if (windowWidth >= App.global.constants.GRID.LG) {
+          diffWidth = windowWidth - 1140;
+        } else {
+          console.log('tab');
+          diffWidth = windowWidth - 940;
+        }
+
+        $tabContets.css({
+          'margin-left': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          'margin-right': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          width: 'calc( 100% + ' + (diffWidth + 20) + 'px)'
+        });
+        console.log('++++' + windowWidth);
+      }
     } else {
       // to mobile
       $tabsTitles.removeClass('active');
       $tabs.removeClass('active');
+    }
+  };
+
+  var matchFunctionLg = function matchFunctionLg(matchmediaParam) {
+    if (matchmediaParam.matches) {
+      if ($tabContets.length > 0) {
+        var windowWidth = $(window).outerWidth();
+        var diffWidth = void 0;
+        if (windowWidth >= App.global.constants.GRID.LG) {
+          diffWidth = windowWidth - 1140;
+        } else {
+          console.log('tab');
+          diffWidth = windowWidth - 940;
+        }
+
+        $tabContets.css({
+          'margin-left': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          'margin-right': Math.floor(-1 * diffWidth / 2 - 10) + 'px',
+          width: 'calc( 100% + ' + (diffWidth + 20) + 'px)'
+        });
+        console.log('++++' + windowWidth);
+      }
     }
   };
 
