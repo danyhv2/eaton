2459c824f35e4f5b6e52eb400e2f85944810c146
Arturo Alfaro <arturo.alfaro@prodigious.cr>
2017-08-22 15:23:26 -0600

    EATON-254

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil/data/view-card-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil/data/view-card-data.js
@@ -14,6 +14,17 @@ use(function () {
         text: 'See how'
       }
     }
+  } else if ((typeof(testdata) != "undefined") && (testdata == "socket")) {
+    data = {
+      title: 'RESIDENTIAL & LIGHT COMMERCIAL BUILDINGS',
+      text: 'PREMERA and COPA 2 gang switched socket with 2 USB ports',
+      imgSrc: '/content/dam/eaton/images/Socket.png',
+      imgAlt: 'Alt Text goes here',
+      cta: {
+        url: 'http://www.eaton.com',
+        text: 'See how'
+      }
+    } 
   } else {
     data = {
       title: 'We make big ideas work',
