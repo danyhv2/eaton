f89cacdf7e70edb453175cec044f259cc7b69fd5
Arturo Alfaro <arturo.alfaro@prodigious.cr>
2017-08-28 17:41:06 -0600

    Updating templates

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
@@ -1,14 +1,33 @@
 'use strict';
 use(function () {
 
-  var data = {
-    image: {
-      mobile: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-      tablet: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-      desktop:'http://lorempixel.com/750/750/technics/Image-size--1000x1000/',
-      altText: "Image Alt Text Goes here"
-    }
-  };
+  var data = {};
+  var testdata = this.testdata;
 
+  if ((typeof(testdata) != "undefined") && (testdata == "t9-secondary-landing-investors-1")) {
+	  
+	  data = {
+			  image: {
+			      mobile: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+			      tablet: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+			      desktop:'http://lorempixel.com/350/350/business/Image-size--350x350/',
+			      altText: "Image Alt Text Goes here"
+			  }
+	  };
+	  
+  } else {
+	  
+	  data = {
+			  image: {
+			      mobile: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+			      tablet: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+			      desktop:'http://lorempixel.com/750/750/technics/Image-size--1000x1000/',
+			      altText: "Image Alt Text Goes here"
+			  }
+	  };
+
+  }
+  
   return data;
+  
 });
