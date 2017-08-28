53cdb66ea738632b15a08fc468a44c48f5809df6
Supriya Bhat <supriya.bhat@razorfish.com>
2017-08-28 16:07:42 -0700

    EATON-252 Image-Component: Data + Add Image rendition template

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
@@ -3,9 +3,9 @@ use(function () {
 
   var data = {
     image: {
-      mobile: "/content/dam/eaton/images/story-board-mobile.png",
-      tablet: "/content/dam/eaton/images/story-board-tablet.png",
-      desktop: "/content/dam/eaton/images/story-board-desktop.png",
+      mobile: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+      tablet: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+      desktop:'http://lorempixel.com/750/750/technics/Image-size--1000x1000/',
       altText: "Image Alt Text Goes here"
     }
   };
