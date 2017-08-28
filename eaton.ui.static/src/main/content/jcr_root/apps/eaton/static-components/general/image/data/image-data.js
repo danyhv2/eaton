edeeb81f9e89afbfec96617df91ed4c50bd28dff
Supriya Bhat <supriya.bhat@razorfish.com>
2017-08-28 15:39:23 -0700

    EATON-252 Image-Component: Initial File Structure

---
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
@@ -0,0 +1,14 @@
+'use strict';
+use(function () {
+
+  var data = {
+    image: {
+      mobile: "/content/dam/eaton/images/story-board-mobile.png",
+      tablet: "/content/dam/eaton/images/story-board-tablet.png",
+      desktop: "/content/dam/eaton/images/story-board-desktop.png",
+      altText: "Image Alt Text Goes here"
+    }
+  };
+
+  return data;
+});
