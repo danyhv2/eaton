19c787782936327f6bb06437fc6ecc8f8eacc16c
Arturo Alfaro <arturo.alfaro@prodigious.cr>
2017-08-28 18:04:46 -0600

    Updating templates

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/general/image/data/image-data.js
@@ -8,20 +8,42 @@ use(function () {
 	  
 	  data = {
 			  image: {
-			      mobile: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-			      tablet: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
+			      mobile: 'http://lorempixel.com/350/350/business/Image-size--350x350/',
+			      tablet: 'http://lorempixel.com/350/350/business/Image-size--350x350/',
 			      desktop:'http://lorempixel.com/350/350/business/Image-size--350x350/',
 			      altText: "Image Alt Text Goes here"
 			  }
 	  };
 	  
+  } else if ((typeof(testdata) != "undefined") && (testdata == "m13")) {
+	  
+	  data = {
+			  image: {
+			      mobile: 'http://lorempixel.com/630/630/technics/Image-size--630x630/',
+			      tablet: 'http://lorempixel.com/460/460/technics/Image-size--460x460/',
+			      desktop:'http://lorempixel.com/460/460/technics/Image-size--460x460/',
+			      altText: "Image Alt Text Goes here"
+			  }
+	  };
+	  
+  } else if ((typeof(testdata) != "undefined") && (testdata == "m20")) {
+	  
+	  data = {
+			  image: {
+			      mobile: 'http://lorempixel.com/630/630/technics/Image-size-630x630/',
+			      tablet: 'http://lorempixel.com/552/552/technics/Image-size-552x552/',
+			      desktop:'http://lorempixel.com/552/552/technics/Image-size-552x552/',
+			      altText: "Image Alt Text Goes here"
+			  }
+	  };
+  
   } else {
 	  
 	  data = {
 			  image: {
-			      mobile: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-			      tablet: '/content/dam/eaton/images/FeaturedStory_Img1_mob.png',
-			      desktop:'http://lorempixel.com/750/750/technics/Image-size--1000x1000/',
+			      mobile: 'http://lorempixel.com/750/750/technics/Image-size--750x750/',
+			      tablet: 'http://lorempixel.com/550/550/technics/Image-size--550x550/',
+			      desktop:'http://lorempixel.com/350/350/technics/Image-size--350x350/',
 			      altText: "Image Alt Text Goes here"
 			  }
 	  };
