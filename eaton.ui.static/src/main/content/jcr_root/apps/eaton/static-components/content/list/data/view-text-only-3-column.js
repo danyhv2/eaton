054e32f27d890dc4867d538a628db221e737c34b
Marcelo Guzman <marcelo.guzman@prodigious.cr>
2017-08-29 07:49:16 -0600

    [M-32] date data added to the  js data file

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-text-only-3-column.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/list/data/view-text-only-3-column.js
@@ -2,7 +2,7 @@
 use(function () {
     var data = {};
     var testdata = this.testdata;
-    
+
     if ((typeof(testdata) != "undefined") && (testdata == "t9-secondary-landing-careers")) {
 
     	data = {
@@ -25,9 +25,9 @@ use(function () {
 	            }
 	        ]
 	    };
-    	
+
     } else {
-    	
+
     	data = {
 			title: "Related Items",
 	        cards: [
@@ -35,16 +35,19 @@ use(function () {
 	                title: 'Company Insights',
 	                text: 'Emergency power system basics',
 	                cta: {url: 'http://www.eaton.com', target: '_self'},
+					date_text: 'November 30, 2016'
 	            },
 	            {
 	                title: 'Utility',
 	                text: 'Eaton Awarded U.S. Army Corps Contract to Modernize Electrical Systems',
 	                cta: {url: 'http://www.eaton.com', target: '_self'},
+ 					date_text: 'December 12, 2015'
 	            },
 	            {
 	                title: 'Community',
 	                text: 'Unlock power and reverse with a modern UPS',
 	                cta: {url: 'http://www.eaton.com', target: '_self'},
+	               	date_text: 'October 12, 2014'
 	            }
 	        ]
 	    };
