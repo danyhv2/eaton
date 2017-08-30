28e6969e2812e0c5a2d7d90c23b503eee633a169
Jorge Meneses <jorge.meneses@razorfish.com>
2017-08-30 14:05:25 -0500

    [CategoryHero] Update test data with property to enable/disable Image Mask

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil-list/data/view-category-hero-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil-list/data/view-category-hero-data.js
@@ -5,7 +5,7 @@ use(function () {
     var testdata = this.testdata;
 
     if ((typeof(testdata) != "undefined") && (testdata == "t5-category-with-list")) {
-
+        data.imageMaskEnabled = false;
     	data.title = "Backup Power UPS";
         data.text = "Eaton is a leading provider of quality backup power UPS, Uninterruptable Power Supply. Eaton UPSs are designed to deliver high quality backup power from desktop PCs to large data centers.";
         data.image = {
@@ -20,10 +20,8 @@ use(function () {
             url: "http://www.eaton.com",
             target: "_self"
         };
-        data.imageMaskEnabled = true;
-
     } else if ((typeof(testdata) != "undefined") && (testdata == "t6-subcategory")) {
-
+        data.imageMaskEnabled = false;
     	data.title = "Network, server & storage UPS";
         data.text = "Networks, servers and storage systems are the central nerve center for bussines today, and it is critical that they are protected by an uninterruptible power supply (UPS).";
         data.image = {
@@ -39,7 +37,7 @@ use(function () {
         };
 
     } else if ((typeof(testdata) != "undefined") && (testdata == "t11-listing-page")) {
-
+        data.imageMaskEnabled = false;
     	data.title = "News Releases";
         data.text = "";
         data.image = {
@@ -55,7 +53,7 @@ use(function () {
         };
 
     } else if ((typeof(testdata) != "undefined") && (testdata == "t9-secondary-landing-investors")) {
-
+        data.imageMaskEnabled = false;
     	data.title = "Investor relations";
         data.text = "Our shareholders invest in us with the intention of benefiting from our long-term success. We take strategic action, create next-generation innovations, and deliver on commitments.";
         data.image = {
@@ -71,7 +69,7 @@ use(function () {
         };
 
     } else if ((typeof(testdata) != "undefined") && (testdata == "t9-secondary-landing-careers")) {
-
+        data.imageMaskEnabled = false;
     	data.title = "Global careers";
         data.text = "At Eaton, we offer more than competitive benefits and growth opportunities for our employees. We create an enviroment where creativity, invention and discovery are reality every day.";
         data.image = {
@@ -86,8 +84,27 @@ use(function () {
             target: "_self"
         };
 
-    } else if ((typeof(testdata) != "undefined") && (testdata == "alignment-center")) {
+    } else if ((typeof(testdata) != "undefined") && (testdata == "with-image-gradient")) {
+        data.imageMaskEnabled = true;
+    	data.title = "Global careers";
+        data.text = "At Eaton, we offer more than competitive benefits and growth opportunities for our employees. We create an enviroment where creativity, invention and discovery are reality every day.";
+        data.image = {
+            mobile: '/content/dam/eaton/images/category-hero-light--mobile.jpg',
+            tablet: '/content/dam/eaton/images/category-hero-light--mobile.jpg',
+            desktop: '/content/dam/eaton/images/category-hero-light--desktop.jpg',
+            altText: '',
+            alignment: 'center'
+        };
+        data.cta = {
+            title: "Company",
+            url: "http://www.eaton.com",
+            target: "_self"
+        };
 
+    }
+
+    else if ((typeof(testdata) != "undefined") && (testdata == "alignment-center")) {
+        data.imageMaskEnabled = true;
     	data.title = "Global careers";
         data.text = "At Eaton, we offer more than competitive benefits and growth opportunities for our employees. We create an enviroment where creativity, invention and discovery are reality every day.";
         data.image = {
@@ -104,7 +121,7 @@ use(function () {
         };
 
     } else if ((typeof(testdata) != "undefined") && (testdata == "alignment-left")) {
-
+        data.imageMaskEnabled = true;
     	data.title = "Global careers";
         data.text = "At Eaton, we offer more than competitive benefits and growth opportunities for our employees. We create an enviroment where creativity, invention and discovery are reality every day.";
         data.image = {
@@ -121,7 +138,7 @@ use(function () {
         };
 
     } else if ((typeof(testdata) != "undefined") && (testdata == "alignment-right")) {
-
+        data.imageMaskEnabled = true;
     	data.title = "Global careers";
         data.text = "At Eaton, we offer more than competitive benefits and growth opportunities for our employees. We create an enviroment where creativity, invention and discovery are reality every day.";
         data.image = {
@@ -138,7 +155,7 @@ use(function () {
         };
 
     } else {
-
+        data.imageMaskEnabled = true;
 	    data.title = "News and Media";
 	    data.text = "We're behind some of the biggest innovations improving safety efficiency and sustainability in ways that matter to customers, businessess and communities. And sometimes that gets people talking.";
 	    data.image = {
@@ -153,8 +170,6 @@ use(function () {
 	        url: "http://www.eaton.com",
 	        target: "_self"
 	    };
-
-        data.imageMaskEnabled = true;
     }
 
     return data;
