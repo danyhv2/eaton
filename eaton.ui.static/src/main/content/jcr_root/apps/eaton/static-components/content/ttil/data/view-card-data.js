1884caf0d4c0fbdcb4925dd5c0f2d98900cb8095
Jorge Meneses <jorge.meneses@razorfish.com>
2017-09-08 16:23:51 -0500

    [EATON-81][EATON-280] Adding image renditions and typography updates to TTIL Card View

--- eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil/data/view-card-data.js
+++ eaton.ui.static/src/main/content/jcr_root/apps/eaton/static-components/content/ttil/data/view-card-data.js
@@ -5,35 +5,50 @@ use(function () {
 
   if ((typeof(testdata) != "undefined") && (testdata == "article-nextsteps")) {
     data = {
-      title: 'Transmissions',
-      text: 'UltraShift Plus Transmissions',
-      imgSrc: '/content/dam/eaton/images/Cityscape_mob.png',
-      imgAlt: 'Alt Text goes here',
-      cta: {
+      title: 'UltraShift Plus Transmissions',
+      text: 'Transmissions',
+      image: {
+        mobile: '/content/dam/eaton/images/next-steps--mobile.jpg',
+        tablet: '/content/dam/eaton/images/next-steps--mobile.jpg',
+        desktop: '/content/dam/eaton/images/next-steps--desktop.jpg',
+        altText: 'Alt Text goes here',
+      },
+      link: {
+        text: 'See how',
         url: 'http://www.eaton.com',
-        text: 'See how'
+        target: '_blank'
       }
     }
   } else if ((typeof(testdata) != "undefined") && (testdata == "socket")) {
     data = {
-      title: 'RESIDENTIAL & LIGHT COMMERCIAL BUILDINGS',
-      text: 'PREMERA and COPA 2 gang switched socket with 2 USB ports',
-      imgSrc: '/content/dam/eaton/images/Socket.png',
-      imgAlt: 'Alt Text goes here',
-      cta: {
+      title: 'PREMERA and COPA 2 gang switched socket with 2 USB ports',
+      text: 'RESIDENTIAL & LIGHT COMMERCIAL BUILDINGS',
+      image: {
+        mobile: '/content/dam/eaton/images/Socket.png',
+        tablet: '/content/dam/eaton/images/Socket.png',
+        desktop: '/content/dam/eaton/images/Socket.png',
+        altText: 'Alt Text goes here',
+      },
+      link: {
+        text: 'See how',
         url: 'http://www.eaton.com',
-        text: 'See how'
+        target: '_self'
       }
-    } 
+    }
   } else {
     data = {
-      title: 'We make big ideas work',
-      text: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
-      imgSrc: '/content/dam/eaton/images/Cityscape_mob.png',
-      imgAlt: 'Alt Text goes here',
-      cta: {
+      title: 'Realize your full potential at Eaton, as you help solve some of the most critical issues.',
+      text: 'We make big ideas work',
+      image: {
+        mobile: '/content/dam/eaton/images/next-steps--mobile.jpg',
+        tablet: '/content/dam/eaton/images/next-steps--mobile.jpg',
+        desktop: '/content/dam/eaton/images/next-steps--desktop.jpg',
+        altText: 'Alt Text goes here',
+      },
+      link: {
+        text: 'See how',
         url: 'http://www.eaton.com',
-        text: 'See how'
+        target: '_blank'
       }
     }
   }
